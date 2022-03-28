e5b8ba329714852b2f402093fdfe8d20121672ba
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-28 17:12:59 -0300

    Added data for members of organization. Fetched from api public

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -1,48 +1,71 @@
-const Team = () => {
-  return (
-    <section className='page-section bg-light' id='team'>
-      <div className='container'>
-        <div className='text-center'>
-          <h2 className='section-heading text-uppercase'>Organizadores</h2>
-        </div>
-        <div className='row'>
-          <div className='col-lg-4'>
-            <div className='team-member'>
-              <img
-                className='mx-auto rounded-circle'
-                src='assets/img/team/1.jpg'
-                alt='...'
-              />
-              <h4>Tino Olivera</h4>
-              <a
-                className='btn btn-dark btn-social mx-2'
-                href='https://www.facebook.com/tino.oliveira.568'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <i className='fab fa-facebook-f' />
-              </a>
-              <a
-                className='btn btn-dark btn-social mx-2'
-                href='https://www.instagram.com/tino.oliveira.568/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <i className='fab fa-instagram' />
-              </a>
-              <a
-                className='btn btn-dark btn-social mx-2'
-                href='https://api.whatsapp.com/send?phone=5493743519395&text=Hola Tino, cómo andás? Quiero consultarte algunos detalles del evento. Soy ....'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <i className='fab fa-whatsapp' />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
-export default Team
+import { useState, useEffect } from 'react';
+import { getMembers } from '../../services/membersServices';
+
+const Members = () => {
+	const [members, setMembers] = useState([]);
+
+	useEffect(() => {
+		const fetchMembers = async () => {
+			const response = await getMembers();
+			setMembers(response.data.data);
+		};
+		fetchMembers();
+	}, []);
+
+	return (
+		<section className='page-section bg-light' id='team'>
+			<div className='container'>
+				<div className='text-center'>
+					<h2 className='section-heading text-uppercase'>Organizadores</h2>
+				</div>
+				<div className='row'>
+					{members &&
+						members.map((item) => {
+							return (
+								<div className='col-lg-4'>
+									<div className='team-member'>
+										{item.image_id.url && (
+											<img
+												className='mx-auto rounded-circle'
+												src={item.image_id.url}
+												alt={item.name}
+											/>
+										)}
+										<h4>{item.name}</h4>
+										{item.urlFacebook && (
+											<a
+												className='btn btn-dark btn-social mx-2'
+												href={item.urlFacebook}
+												target='_blank'
+												rel='noreferrer'>
+												<i className='fa fa-facebook-f' />
+											</a>
+										)}
+										{item.urlInstagram && (
+											<a
+												className='btn btn-dark btn-social mx-2'
+												href={item.urlInstagram}
+												target='_blank'
+												rel='noreferrer'>
+												<i className='fa fa-instagram' />
+											</a>
+										)}
+										{item.phone && (
+											<a
+												className='btn btn-dark btn-social mx-2'
+												href={`https://api.whatsapp.com/send?phone=${item.phone}&text=Hola ${item.name}.`}
+												target='_blank'
+												rel='noreferrer'>
+												<i className='fa fa-whatsapp' />
+											</a>
+										)}
+									</div>
+								</div>
+							);
+						})}
+				</div>
+			</div>
+		</section>
+	);
+};
+export default Members;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LayoutPublic from '../components/LayoutPublic/LayoutPublic';
 import Carousel from '../components/Carousel/Carousel';
+import Members from '../components/Members/Members';
 
 const HomePage = () => {
 	return (
 		<LayoutPublic>
 			<Carousel></Carousel>
+			<Members></Members>
 			{/* 	<AboutUs></AboutUs> */}
 		</LayoutPublic>
 	);
