6e54f379cc9d3aa36f80d6a2c52151c53f67d080
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-28 11:04:05 -0300

    Pass into props data of organization

--- src/components/LayoutPublic/LayoutPublic.js
+++ src/components/LayoutPublic/LayoutPublic.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Navbar from '../Navbar/Nav';
 import Footer from '../Footer/Footer';
-
+import { useState, useEffect } from 'react';
+import { getOrganization } from '../../services/organizationsServices';
 const LayoutPublic = ({ children }) => {
+	const [organization, setOrganization] = useState({});
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await getOrganization(null);
+				setOrganization(response.data.data);
+			} catch (error) {
+				alert(error);
+			}
+		};
+		fetchData();
+	}, []);
 	return (
 		<>
-			<Navbar></Navbar>
-			<main>{children}</main>
-			<Footer></Footer>
+			{organization && (
+				<>
+					<Navbar organization={organization}></Navbar>
+					<main organization={organization}>{children}</main>
+					<Footer organization={organization}></Footer>
+				</>
+			)}
 		</>
 	);
 };
