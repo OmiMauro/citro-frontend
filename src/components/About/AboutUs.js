5afcf9adb94be731d4709201d314a8ff7bbaec57
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-31 18:08:16 -0300

    Refactor component AboutUs for get information from server

--- src/components/About/AboutUs.js
+++ src/components/About/AboutUs.js
@@ -1,86 +1,22 @@
-const Description = () => {
-  return (
-    <section className="page-section" id="services">
-      <div className="container">
-        <div className="text-center ">
-          <h2 className="section-heading text-uppercase">
-            Encuentro Nacional de Autos Citroen
-          </h2>
-          <h3 className="section-subheading text-muted">Citro Rodando</h3>
-        </div>
-        <div className="row text-center fst-italic text-muted">
-          <p>
-            Gran Encuentro de Autos Retro, que en su momento hicieron historia
-            en las calles de nuestro país.
-          </p>
-          <p>
-            Contaremos con la participación de personas y clubes citroneros de
-            distintas provincias, que vendrán a participar de un evento
-            inolvidable a realizarse los días 20 y 21 de noviembre en la
-            provincia de Misiones, dónde se recorrerá distintos lugares de las
-            localidades de San Ignacio, Jardín América, Capioví y Puerto Rico.
-          </p>
-          <p>
-            Visitaremos las Ruinas Jesuíticas de San Ignacio Miní, la casa del
-            escritor Horacio Quiroga, las instalaciones de la Cooperativa Yerba
-            Mate Flor de Jardín, la fábrica de Fécula de Mandioca de Jardín
-            América, los Saltos del Tabay, el complejo Agroturismo Soft, los
-            adornos navideños de Capioví y el aserradero Puerto Maderas.
-          </p>
-          <p>
-            Podrán disfrutar de los Citroen 3cv, 2cv, Mehari, Ami8, Dyane,
-            Forgen, IES, entre muchos otros.
-          </p>
-          <p>
-            Contaremos con las cámaras de nuestros amigos de <b>El Garage TV</b>
-            , que dejarán grabado todo el encuentro.
-          </p>
-          <p>
-            También contaremos con la presencia de <b>Angel Ortiga</b> que
-            exibirá el motor y caja escuela y la presencia de{" "}
-            <b>Pablo Castro</b> que nos mostrará los encendidos e inyección
-            eletrónica.
-          </p>
-          {/* <p>
-            También, queremos agradecer a nuestros auspiciantes,
-            <a
-              className='btn-link text-decoration-none'
-              href='https://www.instagram.com/relampagoel/'
-              target='_blank'
-              rel='noreferrer'
-            >
-              {' '}
-              El Relampago{' '}
-            </a>
-            ,
-            <a
-              className='btn-link text-decoration-none'
-              href='https://www.instagram.com/indualaberturas/'
-              target='_blank'
-              rel='noreferrer'
-            >
-              {' '}
-              Indual Aberturas,
-            </a>
-            <a
-              className='btn-link text-decoration-none'
-              href='https://www.instagram.com/mundo_piscinas/'
-              target='_blank'
-              rel='noreferrer'
-            >
-              {' '}
-              Mundo Piscinas
-            </a>
-            , que con su apoyo, hacen posible que éste evento se haga realidad
-            para que podamos disfrutar con nuestra Gran Familia de Citroneros...
-          </p>
- */}
-          <p className="text-muted text-center">
-            Esperamos tú participación...{" "}
-          </p>
-        </div>
-      </div>
-    </section>
-  );
-};
-export default Description;
+const AboutUs = (props) => {
+	console.log('about us', props.organization)
+	const { aboutUs, name, welcomeText } = props.organization
+	const splitedDescription = aboutUs ? aboutUs.split('. ') : ['']
+	return (
+		<section className='page-section' id='services'>
+			<div className='container'>
+				<div className='text-center '>
+					<h2 className='section-heading text-uppercase'>{name}</h2>
+					<h3 className='section-subheading text-muted'>{welcomeText}</h3>
+				</div>
+				<div className='row text-center fst-italic text-muted'>
+					{splitedDescription &&
+						splitedDescription.map((paragraph) => {
+							return <p>{paragraph}.</p>
+						})}
+				</div>
+			</div>
+		</section>
+	)
+}
+export default AboutUs
