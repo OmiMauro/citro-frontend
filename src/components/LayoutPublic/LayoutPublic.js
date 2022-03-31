a0ab74ca342183b5dc336f539b35c1f077559941
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-31 18:09:20 -0300

    Remove useEffect to component HomePage for get information about organization

--- src/components/LayoutPublic/LayoutPublic.js
+++ src/components/LayoutPublic/LayoutPublic.js
@@ -1,33 +1,18 @@
-import React from 'react';
-import Navbar from '../Navbar/Nav';
-import Footer from '../Footer/Footer';
-import { useState, useEffect } from 'react';
-import { getOrganization } from '../../services/organizationsServices';
-const LayoutPublic = ({ children }) => {
-	const [organization, setOrganization] = useState({});
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await getOrganization(null);
-				setOrganization(response.data.data);
-			} catch (error) {
-				alert(error);
-			}
-		};
-		fetchData();
-	}, []);
+import React from 'react'
+import Navbar from '../Navbar/Nav'
+import Footer from '../Footer/Footer'
+const LayoutPublic = ({ children, organization }) => {
 	return (
 		<>
 			{organization && (
 				<>
 					<Navbar organization={organization}></Navbar>
-					<main organization={organization}>{children}</main>
+					<main>{children}</main>
 					<Footer organization={organization}></Footer>
 				</>
 			)}
 		</>
-	);
-};
+	)
+}
 
-export default LayoutPublic;
+export default LayoutPublic
