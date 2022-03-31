e5dfa046550d3f12a8f27d5352c0d0ef82de92ee
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-31 18:10:19 -0300

    Added component AboutUs

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import LayoutPublic from '../components/LayoutPublic/LayoutPublic';
-import Carousel from '../components/Carousel/Carousel';
-import Members from '../components/Members/Members';
+import React from 'react'
+import LayoutPublic from '../components/LayoutPublic/LayoutPublic'
+import Carousel from '../components/Carousel/Carousel'
+import Members from '../components/Members/Members'
+import AboutUs from '../components/About/AboutUs'
+const HomePage = (props) => {
+	const { organization } = props
 
-const HomePage = () => {
 	return (
-		<LayoutPublic>
+		<LayoutPublic organization={organization}>
 			<Carousel></Carousel>
+			<AboutUs organization={organization}></AboutUs>
 			<Members></Members>
-			{/* 	<AboutUs></AboutUs> */}
 		</LayoutPublic>
-	);
-};
+	)
+}
 
-export default HomePage;
+export default HomePage
