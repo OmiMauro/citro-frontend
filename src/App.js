ea625b5f7317a9af65d5ca9ed699f76ad042ded5
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-04 10:14:12 -0300

    Added Routes to App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
 import LayoutPublic from './components/LayoutPublic/LayoutPublic'
-import publicRoute from './components/routes/publicRoutes'
-import SuperRoute from './components/routes'
 import HomePage from './pages/HomePage'
 import { useState, useEffect } from 'react'
 import { getOrganization } from './services/organizationsServices'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import Error404 from './pages/Error404'
+import Error404Page from './pages/Error404'
 const App = () => {
 	const publics = ['/', '/activities', '/login', '/register']
 	const privates = ['/backoffice', '/backofice/*']
@@ -24,27 +27,43 @@ const App = () => {
 	}, [])
 	return (
 		<>
-			<HomePage organization={organization}></HomePage>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					{/* public routes */}
+					<Route
+						path='/home'
+						element={<HomePage organization={organization}></HomePage>}
+					/>
+					<Route
+						path='/login'
+						element={<LoginPage organization={organization} />}
+					/>
+					<Route path='register' element={<RegisterPage />} />
 
-			{/* 	<Routes>
-				<Route exact path={publics}>
-					<LayoutPublic></LayoutPublic>
-					<LayoutPublic>
-						<div className='container'>
-							console.log(publicRoute);
-							{publicRoute.map(({ path, component, exact }) => {
-								return (
-									<SuperRoute
-										key={path}
-										component={component}
-										exact={exact}
-										path={path}></SuperRoute>
-								);
-							})}
-						</div>
-					</LayoutPublic>
+					{/* we want to protect these routes */}
+					{/* <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+						<Route path='/' element={<Home />} />
+					</Route>
+
+					<Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
+						<Route path='editor' element={<Editor />} />
+					</Route>
+
+					<Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+						<Route path='admin' element={<Admin />} />
+					</Route>
+
+					<Route
+						element={
+							<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />
+						}>
+						<Route path='lounge' element={<Lounge />} />
+					</Route>
+ */}
+					{/* catch all */}
+					<Route path='*' element={<Error404Page />} />
 				</Route>
-			</Routes> */}
+			</Routes>
 		</>
 	)
 }
