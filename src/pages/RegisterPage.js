aef83fb63e8b3665dd244c8562e42ef30ba95a97
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-04 10:12:18 -0300

    Remove Title from page Register

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import Titles from '../components/Titles/Titles';
-import Register from '../components/Auth/Register';
-import LayoutPublic from '../components/LayoutPublic/LayoutPublic';
+import Register from '../components/Auth/Register'
+import LayoutPublic from '../components/LayoutPublic/LayoutPublic'
 
 const RegisterPage = () => {
 	return (
 		<LayoutPublic>
-			<Titles title='Registarse'></Titles>
 			<Register></Register>
 		</LayoutPublic>
-	);
-};
+	)
+}
 
-export default RegisterPage;
+export default RegisterPage
