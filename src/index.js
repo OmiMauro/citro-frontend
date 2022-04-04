809650bc9b4b42bfe55ca4f9273b34188867781a
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-04 10:10:15 -0300

    Added modules for redux. Create store and middleware thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/css/index.css';
-import './assets/css/styles.css';
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/css/index.css'
+import './assets/css/styles.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import store from './redux/store'
+import { Provider } from 'react-redux'
+import App from './App'
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/*' element={<App />} />
+				</Routes>
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
 	document.getElementById('root')
-);
+)
