065aec723e5c3aa5935bf5ff8acf00fc2ccb7e84
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-10 22:58:10 -0300

    Added dispatches to state for Login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,70 +1,94 @@
 import './auth-styles.css'
-
+import { selectorAuth, logged } from '../../redux/slices/auth-slice'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Spinner from '../Spinner/Spinner'
 const Login = () => {
+	const [values, setValues] = useState({
+		email: '',
+		password: ''
+	})
+	const { email, password, submitted } = values
+	const { auth, user, isLoading } = useSelector(selectorAuth)
+	const dispatch = useDispatch()
+	const handleChange = (e) => {
+		const { name, value } = e.target
+		setValues({ ...values, [name]: value })
+	}
+	useEffect(() => {}, [auth])
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const body = {
+			email,
+			password
+		}
+		dispatch(logged(body))
+	}
 	return (
 		<>
 			<div id='layoutAuthentication'>
 				<div id='layoutAuthentication_content'>
 					<div className='container'>
-						<div className='row justify-content-center'>
-							<div className='col-lg-5'>
-								<div className='card shadow-lg border-0 rounded-lg mt-5'>
-									<div className='card-header'>
-										<h3 className='text-center font-weight-light my-4'>
-											Iniciar Sesion
-										</h3>
-									</div>
-									<div className='card-body'>
-										<form>
-											<div className='form-floating mb-3'>
-												<input
-													className='form-control'
-													id='inputEmail'
-													type='email'
-													placeholder='correo@ejemplo.com'
-												/>
-												<label for='inputEmail'>Correo electrónico</label>
+						{isLoading ? (
+							<Spinner></Spinner>
+						) : (
+							<div className='row justify-content-center'>
+								<div className='col-lg-5'>
+									<div className='card shadow-lg border-0 rounded-lg mt-5'>
+										<div className='card-header'>
+											<h3 className='text-center font-weight-light my-4'>
+												Iniciar Sesion
+											</h3>
+										</div>
+										<div className='card-body'>
+											<form onSubmit={handleSubmit}>
+												<div className='form-floating mb-3'>
+													<input
+														className='form-control'
+														id='email'
+														type='email'
+														value={email}
+														name='email'
+														placeholder='correo@ejemplo.com'
+														onChange={handleChange}
+													/>
+													<label htmlFor='email'>Correo electrónico</label>
+												</div>
+												<div className='form-floating mb-3'>
+													<input
+														className='form-control'
+														id='password'
+														value={password}
+														type='password'
+														name='password'
+														onChange={handleChange}
+														placeholder='Contraseña'
+													/>
+													<label htmlFor='password'>Contraseña</label>
+												</div>
+
+												<div className='d-flex align-items-center justify-content-between mt-4 mb-0'>
+													<Link className='small' to='/forgotPassword'>
+														Olvidó su contraseña?
+													</Link>
+												</div>
+												<button className='btn btn-primary'>
+													Iniciar sesión
+												</button>
+											</form>
+										</div>
+										<div className='card-footer text-center py-3'>
+											<div className='small'>
+												<Link to='/register'>
+													Necesitas una nueva cuenta? Registrarse
+												</Link>
 											</div>
-											<div className='form-floating mb-3'>
-												<input
-													className='form-control'
-													id='inputPassword'
-													type='password'
-													placeholder='Contraseña'
-												/>
-												<label for='inputPassword'>Contraseña</label>
-											</div>
-											<div className='form-check mb-3'>
-												<input
-													className='form-check-input'
-													id='inputRememberPassword'
-													type='checkbox'
-													value=''
-												/>
-												<label
-													className='form-check-label'
-													for='inputRememberPassword'>
-													Recordar contraseña?
-												</label>
-											</div>
-											<div className='d-flex align-items-center justify-content-between mt-4 mb-0'>
-												<a className='small' href='password.html'>
-													Olvidó su contraseña?
-												</a>
-												<a className='btn btn-primary' href='index.html'>
-													Iniciar sesion
-												</a>
-											</div>
-										</form>
-									</div>
-									<div className='card-footer text-center py-3'>
-										<div className='small'>
-											<a>Necesitas una nueva cuenta? Registrarse</a>
 										</div>
 									</div>
 								</div>
 							</div>
-						</div>
+						)}
 					</div>
 				</div>
 			</div>
