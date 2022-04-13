b10819967ea5da818a46519638e56cb799bc6135
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-12 23:26:09 -0300

    Remove Nav from page of backoffice

--- src/pages/BackofficePage.js
+++ src/pages/BackofficePage.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import LayoutPublic from '../components/LayoutPublic/LayoutPublic'
-import BackofficeNav from '../components/Backoffice/BackofficeNav/BackofficeNav'
 import Table from '../components/Backoffice/Tables/Tables'
 import SideNav from '../components/Backoffice/SideNav/SideNav'
 
 const BackofficePage = () => {
 	return (
 		<>
-			<div className='sb-nav-fixed'>
-				<BackofficeNav></BackofficeNav>
-				<div id='layoutSidenav'>
+			<div className='container-fluid'>
+				<div className='row flex-nowrap'>
 					<SideNav></SideNav>
 					<Table></Table>
 				</div>
