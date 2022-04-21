022a2cde4cf0ab166f589af8c44fd0be4b2dad2d
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-21 11:07:44 -0300

    Create component memberListContainer

---
+++ src/containers/Members/MembersListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MembersList from '../../components/Members/MembersList'
+const MembersListContainer = () => {
+	return (
+		<div>
+			<p>MembersListContainer</p>
+			<MembersList></MembersList>
+		</div>
+	)
+}
+
+export default MembersListContainer
