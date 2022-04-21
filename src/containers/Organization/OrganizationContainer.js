6e7abc9e39b64877667471e573a9ffb6bc2eaa9f
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-21 10:59:59 -0300

    Added Component for manage data of organization

---
+++ src/containers/Organization/OrganizationContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Organization from '../../components/Organization/Organization'
+const OrganizationContainer = () => {
+	return (
+		<>
+			<div className='col py-3'>
+				<p>This is container for organization</p>
+				<Organization></Organization>
+			</div>
+		</>
+	)
+}
+
+export default OrganizationContainer
---
+++ src/containers/Organization/OrganizationFormContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import OrganizationForm from '../../components/Organization/OrganizationForm'
+const OrganizationFormContainer = () => {
+	return (
+		<div>
+			<p>OrganizationFormContainer</p>
+			<OrganizationForm></OrganizationForm>
+		</div>
+	)
+}
+
+export default OrganizationFormContainer
