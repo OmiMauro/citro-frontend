6362267da811cfb812f1bc103a8815b18e4420d2
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-22 11:05:36 -0300

    remove logs

--- src/services/organizations-services.js
+++ src/services/organizations-services.js
@@ -1,12 +1,8 @@
 import { get } from './public-api-services'
 
 const ENDPOINT = process.env.REACT_APP_API_ORGANIZATIONS_ENDPOINT
-const BASE_URL = process.env.REACT_APP_API_BASE_URL
 export const getOrganization = async (search = null) => {
 	try {
-		console.log('ENDPOINT', ENDPOINT)
-		console.log('BASE URL', BASE_URL)
-
 		return await get(ENDPOINT)
 	} catch (error) {
 		console.log(error)
