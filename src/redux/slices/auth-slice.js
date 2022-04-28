96a128b4dc66ace0df51d608b59485fa97ccfc4d
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-28 09:40:34 -0300

    Added param rejectWithValue when receive a error from API

--- src/redux/slices/auth-slice.js
+++ src/redux/slices/auth-slice.js
@@ -2,18 +2,33 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { login, register } from '../../services/auth-services'
 import { STATUS } from '../constants/action-types'
 
-export const logged = createAsyncThunk('authLogin/post', async (data) => {
-	const response = await login(data)
-	localStorage.setItem('token', response.data.data.token)
-	return response.data.data
-})
+export const logged = createAsyncThunk(
+	'authLogin/post',
+	async (data, { rejectWithValue }) => {
+		try {
+			const response = await login(data)
+			if (response) {
+				localStorage.setItem('token', response.data.data.token)
+				return response.data.data
+			}
+		} catch (error) {
+			return rejectWithValue(error)
+		}
+	}
+)
 
 export const registered = createAsyncThunk(
 	'authRegister/post',
-	async (data) => {
-		const response = await register(data)
-		localStorage.setItem('token', response.data.data.token)
-		return response.data.data
+	async (data, { rejectWithValue }) => {
+		try {
+			const response = await register(data)
+			if (response) {
+				localStorage.setItem('token', response.data.data.token)
+				return response.data.data
+			}
+		} catch (error) {
+			return rejectWithValue(error)
+		}
 	}
 )
 const authSlice = createSlice({
@@ -23,7 +38,8 @@ const authSlice = createSlice({
 		user: {},
 		token: '',
 		isLoading: false,
-		status: null
+		status: null,
+		errors: []
 	},
 	reducers: {
 		logout: (state) => {
@@ -43,15 +59,23 @@ const authSlice = createSlice({
 			state.auth = true
 			state.user = action.payload.user
 			state.token = action.payload.token
+			state.errors = []
 			state.isLoading = false
 		},
-		[logged.rejected]: (state) => {
+		[logged.rejected]: (state, action) => {
 			state.status = STATUS.FAILED
 			state.isLoading = false
+			state.errors = action.payload.response.data.errors
 		},
 		[registered.pending]: (state) => {
 			state.status = STATUS.PENDING
 			state.isLoading = true
+		},
+
+		[registered.rejected]: (state, action) => {
+			state.status = STATUS.FAILED
+			state.isLoading = true
+			state.errors = action.payload.response.data.errors
 		},
 		[registered.fulfilled]: (state, action) => {
 			state.status = STATUS.SUCCESSFUL
@@ -59,10 +83,7 @@ const authSlice = createSlice({
 			state.user = action.payload.user
 			state.token = action.payload.token
 			state.isLoading = false
-		},
-		[registered.rejected]: (state) => {
-			state.status = STATUS.FAILED
-			state.isLoading = false
+			state.errors = []
 		}
 	}
 })
