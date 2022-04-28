13bca09f95d7dd62f1ac1976ce7f5fffc262ca02
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-28 19:15:33 -0300

    Added removePicture, addPictures y manage errors when happen in server

--- src/redux/slices/galery-slice.js
+++ src/redux/slices/galery-slice.js
@@ -1,18 +1,50 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { getGalery } from '../../services/galery-services'
+import {
+	getGalery,
+	newPictures,
+	deletePicture
+} from '../../services/galery-services'
 import { STATUS } from '../constants/action-types'
 
-export const fetchGalery = createAsyncThunk('galery/get', async () => {
-	const {
-		data: { data }
-	} = await getGalery()
-	return data
-})
+export const fetchGalery = createAsyncThunk(
+	'galery/get',
+	async ({ rejectWithValue }) => {
+		try {
+			const response = await getGalery()
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+const addImagesToGalery = createAsyncThunk(
+	'galery/post',
+	async (data, { rejectWithValue }) => {
+		try {
+			const response = await newPictures(data)
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+const removePicture = createAsyncThunk(
+	'galery/delete',
+	async (id, { rejectWithValue }) => {
+		try {
+			const response = await deletePicture(id)
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
 const galerySlice = createSlice({
 	name: 'galery',
 	initialState: {
 		galery: [],
-		status: null
+		status: null,
+		errors: []
 	},
 	extraReducers: {
 		[fetchGalery.pending]: (state) => {
@@ -21,9 +53,35 @@ const galerySlice = createSlice({
 		[fetchGalery.fulfilled]: (state, action) => {
 			state.status = STATUS.SUCCESSFUL
 			state.galery = action.payload
+			errors: []
 		},
-		[fetchGalery.rejected]: (state) => {
+		[fetchGalery.rejected]: (state, { payload }) => {
 			state.status = STATUS.FAILED
+			state.errors = payload.data.errors
+		},
+		[addImagesToGalery.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[addImagesToGalery.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.galery = payload
+			errors: []
+		},
+		[addImagesToGalery.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload.data.errors
+		},
+		[removePicture.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[removePicture.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.galery = state.galery.filter((id) => id !== payload)
+			errors: []
+		},
+		[removePicture.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload.data.errors
 		}
 	}
 })
--- src/services/galery-services.js
+++ src/services/galery-services.js
@@ -1,11 +1,14 @@
+import { post, remove } from './private-api-services'
 import { get } from './public-api-services'
 
 const ENDPOINT = process.env.REACT_APP_API_GALERY_ENDPOINT
 
 export const getGalery = async (search = null) => {
-	try {
-		return await get(ENDPOINT)
-	} catch (error) {
-		alert(error)
-	}
+	return await get(ENDPOINT)
+}
+export const newPictures = async (data) => {
+	return await post(ENDPOINT, data)
+}
+export const deletePicture = async (id) => {
+	return await remove(ENDPOINT, id)
 }
