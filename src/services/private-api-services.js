3494d158ed1fc5ad85134fb46597f0ca7d0d2819
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-28 19:17:25 -0300

    Added interceptor req for bearear token in Autorization of services privates

---
+++ src/services/private-api-services.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import config from './axios-config'
+
+const instance = axios.create(config)
+
+const token = localStorage.getItem('token')
+
+instance.interceptors.request.use(
+	(config) => {
+		config.headers.Authorization = token ? `Bearer ${token}` : {}
+		return config
+	},
+	(error) => error
+)
+
+instance.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		return Promise.reject(error)
+	}
+)
+
+const get = async (url, search = null, id = null) => {
+	let processedURL = id ? `${url}/${id}` : url
+	return await instance.get(url, { params: search })
+}
+
+const post = async (url, data) => await instance.post(url, data)
+
+const put = async (url, data, id) => await instance.put(`${url}/${id}`, data)
+
+const remove = async (url, id) => await instance.delete(`${url}/${id}`)
+
+export { get, post, put, remove }
