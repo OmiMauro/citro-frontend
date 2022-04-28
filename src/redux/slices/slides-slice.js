2e32cf32eb490c0c175df2b9eb3e06ffdbe560bd
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-28 19:16:46 -0300

    Added getSlides, removeSlide, getSlideById and newSlide. Manage errors when happen in server

--- src/redux/slices/slides-slice.js
+++ src/redux/slices/slides-slice.js
@@ -1,29 +1,133 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { getSlides } from '../../services/slides-services'
+import {
+	getSlides,
+	getSlideById,
+	newSlide,
+	deleteSlide,
+	putSlide
+} from '../../services/slides-services'
 import { STATUS } from '../constants/action-types'
 
-export const fetchSlides = createAsyncThunk('slides/get', async () => {
-	const {
-		data: { data }
-	} = await getSlides()
-	return data
-})
+export const fetchSlides = createAsyncThunk(
+	'slides/get',
+	async ({ rejectWithValue }) => {
+		try {
+			const response = await getSlides()
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+export const fetchSlideById = createAsyncThunk(
+	'slides/getById',
+	async (id, { rejectWithValue }) => {
+		try {
+			const response = await getSlideById(id)
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+export const createSlide = createAsyncThunk(
+	'slides/create',
+	async (data, { rejectWithValue }) => {
+		try {
+			const response = await newSlide(data)
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+export const updateSlide = createAsyncThunk(
+	'slides/put',
+	async (data, { rejectWithValue }) => {
+		try {
+			const response = await putSlide(data, data._id)
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+export const removeSlide = createAsyncThunk(
+	'slides/delete',
+	async (id, { rejectWithValue }) => {
+		try {
+			const response = await deleteSlide(id)
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
 const slidesSlice = createSlice({
 	name: 'slides',
 	initialState: {
 		slides: [],
-		status: null
+		slide: {},
+		status: null,
+		errors: []
 	},
 	extraReducers: {
 		[fetchSlides.pending]: (state) => {
 			state.status = STATUS.PENDING
 		},
-		[fetchSlides.fulfilled]: (state, action) => {
+		[fetchSlides.fulfilled]: (state, { payload }) => {
 			state.status = STATUS.SUCCESSFUL
-			state.slides = action.payload
+			state.slides = payload
+			state.errors = []
 		},
-		[fetchSlides.rejected]: (state) => {
+		[fetchSlides.rejected]: (state, { payload }) => {
 			state.status = STATUS.FAILED
+			state.errors = payload.errors
+		},
+		[fetchSlideById.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[fetchSlideById.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.slide = payload
+		},
+		[fetchSlideById.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload.errors
+		},
+		[createSlide.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[createSlide.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.slide = payload
+			state.slides = state.slides.push(payload)
+		},
+		[createSlide.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload.errors
+		},
+		[updateSlide.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[updateSlide.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.slide = payload
+		},
+		[updateSlide.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload.errors
+		},
+		[removeSlide.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[removeSlide.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.slides = state.slides.filter((id) => id !== payload)
+		},
+		[removeSlide.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload.errors
 		}
 	}
 })
--- src/services/slides-services.js
+++ src/services/slides-services.js
@@ -1,11 +1,19 @@
 import { get } from './public-api-services'
-
+import { get as getPrivate, post, put, remove } from './private-api-services'
 const ENDPOINT = process.env.REACT_APP_API_SLIDES_ENDPOINT
 
 export const getSlides = async (search = null) => {
-	try {
-		return await get(ENDPOINT)
-	} catch (error) {
-		alert(error)
-	}
+	return await get(ENDPOINT)
+}
+export const getSlideById = async (id) => {
+	return await getPrivate(ENDPOINT, null, id)
+}
+export const newSlide = async (data) => {
+	return await post(ENDPOINT, data)
+}
+export const putSlide = async (data, id) => {
+	return await put(ENDPOINT, data, id)
+}
+export const deleteSlide = async (id) => {
+	return await remove(ENDPOINT, id)
 }
