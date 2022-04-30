adc8930a724e6623acbbd097c181c93402df33c4
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-30 11:06:18 -0300

    Service for update data of organization

--- src/services/organizations-services.js
+++ src/services/organizations-services.js
@@ -1,10 +1,10 @@
 import { get } from './public-api-services'
-
+import { put } from './private-api-services'
 const ENDPOINT = process.env.REACT_APP_API_ORGANIZATIONS_ENDPOINT
 export const getOrganization = async (search = null) => {
-	try {
-		return await get(ENDPOINT)
-	} catch (error) {
-		console.log(error)
-	}
+	return await get(ENDPOINT)
+}
+
+export const putOrganization = async (data) => {
+	return await put(ENDPOINT, data)
 }
