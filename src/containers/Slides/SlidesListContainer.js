0813e03243431c6f747211900c4632f179a6b078
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-30 19:21:38 -0300

    Add dispatch for get all slides

--- src/containers/Slides/SlidesListContainer.js
+++ src/containers/Slides/SlidesListContainer.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import SlidesList from '../../components/Slides/SlidesList'
+import { useEffect } from 'react'
+import { selectorSlides, fetchSlides } from '../../redux/slices/slides-slice'
+import { useDispatch, useSelector } from 'react-redux'
+
 const SlidesListContainer = () => {
+	const { slides, status, errors } = useSelector(selectorSlides)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(fetchSlides())
+	}, [dispatch])
+
+	const handleDelete = () => {}
 	return (
 		<>
-			<p>Slides Container</p>
-			<SlidesList />
+			<div className='col'>
+				<h3 className='text-center fw-bold mb-5 mx-1 mx-md-4 mt-4'>
+					Lista de Slides
+				</h3>
+				<SlidesList slides={slides} handleDelete={handleDelete} />
+			</div>
 		</>
 	)
 }
