475a1fc9e38ddd22076f94c4dfab7d7a2c19cea2
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-30 10:41:33 -0300

    Added table for view all members data

--- src/components/Members/MembersList.js
+++ src/components/Members/MembersList.js
@@ -1,7 +1,69 @@
 import React from 'react'
-
-const MembersList = () => {
-	return <div>MembersList</div>
+import { Link } from 'react-router-dom'
+const MembersList = ({ members, onDelete }) => {
+	return (
+		<>
+			<div className='table-responsive'>
+				<table className='table align-middle table-hover table-striped table-list table-bordered  table-sm'>
+					<thead className='thead-list'>
+						<tr>
+							<th>Nombre</th>
+							<th>Apellido</th>
+							<th>Facebook</th>
+							<th>Instagram</th>
+							<th>Whatsapp</th>
+							<th>Foto</th>
+							<th>Acciones</th>
+						</tr>
+					</thead>
+					<tbody>
+						{members.map((member) => {
+							return (
+								<tr key={member._id}>
+									<td className='align-middle'>
+										<p className='mt-3'>{member.name}</p>
+									</td>
+									<td className='align-middle'>
+										<p className='mt-3'>{member.lastname}</p>
+									</td>
+									<td className='align-middle'>
+										<a className='mt-3 text-black'>{member.urlFacebook}</a>
+									</td>
+									<td className='align-middle'>
+										<a className='mt-3 text-black'>{member.urlInstagram}</a>
+									</td>
+									<td className='align-middle'>
+										<a className='mt-3 text-black'>{member.urlWhatsapp}</a>
+									</td>
+									<td className='align-middle'>
+										<img
+											alt='member'
+											width={'40px'}
+											className='img-table rounded mx-auto d-block '
+											src={member.image_id.url}
+										/>
+									</td>
+									<td className='align-middle'>
+										<Link to={`/backoffice/members/edit/${member._id}`}>
+											<button className='btn-list btn-edit'>
+												<i className='fas fa-pencil-alt' />
+											</button>
+										</Link>
+										<button
+											className='btn-list btn-delete'
+											title='Eliminar'
+											onClick={() => onDelete(member._id)}>
+											<i className='fa fa-trash-alt' />
+										</button>
+									</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</div>
+		</>
+	)
 }
 
 export default MembersList
--- src/containers/Members/MembersListContainer.js
+++ src/containers/Members/MembersListContainer.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MembersList from '../../components/Members/MembersList'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectorMembers, fetchMembers } from '../../redux/slices/members-slice'
+
 const MembersListContainer = () => {
+	const { members, status, errors } = useSelector(selectorMembers)
+	const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(fetchMembers())
+	}, [dispatch])
+
 	return (
-		<div>
-			<p>MembersListContainer</p>
-			<MembersList></MembersList>
+		<div className='col'>
+			<h3 className='text-center fw-bold mb-5 mx-1 mx-md-4 mt-4'>
+				Lista de los organizadores
+			</h3>
+			<MembersList members={members} />
 		</div>
 	)
 }
