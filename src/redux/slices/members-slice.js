739fbefe52fc345533001d9fc564ac6b9c5efc4d
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-30 11:03:38 -0300

    Slices for CRUD of members and manage errors when happen in server

--- src/redux/slices/members-slice.js
+++ src/redux/slices/members-slice.js
@@ -1,29 +1,133 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { getMembers } from '../../services/members-services'
+import {
+	getMembers,
+	getMemberById,
+	newMember,
+	deleteMember,
+	editMember
+} from '../../services/members-services'
 import { STATUS } from '../constants/action-types'
 
-export const fetchMembers = createAsyncThunk('members/get', async () => {
-	const {
-		data: { data }
-	} = await getMembers()
-	return data
-})
+export const fetchMembers = createAsyncThunk(
+	'members/get',
+	async (param = null, { rejectWithValue }) => {
+		try {
+			const response = await getMembers()
+			if (response) return response.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+export const fetchMemberById = createAsyncThunk(
+	'members/getById',
+	async (id, { rejectWithValue }) => {
+		try {
+			const response = await getMemberById(id)
+			if (response) return response.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+export const createMember = createAsyncThunk(
+	'members/post',
+	async (data, { rejectWithValue }) => {
+		try {
+			const response = await newMember(data)
+			if (response) return response.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+export const putMember = createAsyncThunk(
+	'members/put',
+	async (data, { rejectWithValue }) => {
+		try {
+			const response = await editMember(data, data._id)
+			if (response) return response.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+export const removeMember = createAsyncThunk(
+	'members/delete',
+	async (id, { rejectWithValue }) => {
+		try {
+			const response = await deleteMember(id)
+			if (response) return response.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
 const membersSlice = createSlice({
 	name: 'members',
 	initialState: {
 		members: [],
-		status: null
+		member: {},
+		status: null,
+		errors: []
 	},
 	extraReducers: {
 		[fetchMembers.pending]: (state) => {
 			state.status = STATUS.PENDING
 		},
-		[fetchMembers.fulfilled]: (state, action) => {
+		[fetchMembers.fulfilled]: (state, { payload }) => {
 			state.status = STATUS.SUCCESSFUL
-			state.members = action.payload
+			state.members = payload.data
 		},
-		[fetchMembers.rejected]: (state) => {
+		[fetchMembers.rejected]: (state, action) => {
 			state.status = STATUS.FAILED
+			state.errors = action.payload.errors
+		},
+		[fetchMemberById.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[fetchMemberById.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.member = payload
+			state.errors = []
+		},
+		[fetchMemberById.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload.errors
+		},
+		[createMember.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[createMember.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.member = payload
+			state.members = state.members.push(payload)
+		},
+		[createMember.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload
+		},
+		[putMember.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[putMember.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.member = payload
+		},
+		[putMember.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload
+		},
+		[removeMember.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[removeMember.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.member = payload
+		},
+		[removeMember.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload
 		}
 	}
 })
