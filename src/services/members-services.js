d4d1dac4eadcf29868cd8bafb1503c6d01b34e51
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-30 11:06:01 -0300

    Add services for Members CRUD

--- src/services/members-services.js
+++ src/services/members-services.js
@@ -1,11 +1,22 @@
 import { get } from './public-api-services'
-
+import { get as getPrivate, post, put, remove } from './private-api-services'
 const ENDPOINT = process.env.REACT_APP_API_MEMBERS_ENDPOINT
 
 export const getMembers = async (search = null) => {
-	try {
-		return await get(ENDPOINT)
-	} catch (error) {
-		alert(error)
-	}
+	return await get(ENDPOINT)
+}
+export const getMemberById = async (id) => {
+	return await getPrivate(ENDPOINT, null, id)
+}
+
+export const newMember = async (data) => {
+	return await post(ENDPOINT, data)
+}
+
+export const editMember = async (data, id) => {
+	return await put(ENDPOINT, data, id)
+}
+
+export const deleteMember = async (id) => {
+	return await remove(ENDPOINT, id)
 }
