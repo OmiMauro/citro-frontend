b40b5af3ec62dccd0bb0671fecac1d8e9b9a28fc
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-30 11:04:51 -0300

    Slice for edit organization and manage errors when happen in server

--- src/redux/slices/organization-slice.js
+++ src/redux/slices/organization-slice.js
@@ -1,32 +1,62 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { getOrganization } from '../../services/organizations-services'
+import {
+	getOrganization,
+	putOrganization
+} from '../../services/organizations-services'
 import { STATUS } from '../constants/action-types'
 
 export const fetchOrganization = createAsyncThunk(
 	'organization/get',
-	async () => {
-		const {
-			data: { data }
-		} = await getOrganization()
-		return data
+	async (param, { rejectWithValue }) => {
+		try {
+			const response = await getOrganization()
+			if (response) return response.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
 	}
 )
+export const editOrganization = createAsyncThunk(
+	'organization/put',
+	async (data, { rejectWithValue }) => {
+		try {
+			const response = await putOrganization(data)
+			if (response) return response.data.data
+		} catch (error) {
+			return rejectWithValue(error.response.data)
+		}
+	}
+)
+
 const organizationSlice = createSlice({
 	name: 'organization',
 	initialState: {
 		organization: {},
-		status: null
+		status: null,
+		errors: []
 	},
 	extraReducers: {
 		[fetchOrganization.pending]: (state) => {
 			state.status = STATUS.PENDING
 		},
-		[fetchOrganization.fulfilled]: (state, action) => {
+		[fetchOrganization.fulfilled]: (state, { payload }) => {
 			state.status = STATUS.SUCCESSFUL
-			state.organization = action.payload
+			state.organization = payload.data
 		},
-		[fetchOrganization.rejected]: (state) => {
+		[fetchOrganization.rejected]: (state, { payload }) => {
 			state.status = STATUS.FAILED
+			state.errors = payload.errors
+		},
+		[editOrganization.pending]: (state) => {
+			state.status = STATUS.PENDING
+		},
+		[editOrganization.fulfilled]: (state, { payload }) => {
+			state.status = STATUS.SUCCESSFUL
+			state.organization = payload.data
+		},
+		[editOrganization.rejected]: (state, { payload }) => {
+			state.status = STATUS.FAILED
+			state.errors = payload.errors
 		}
 	}
 })
