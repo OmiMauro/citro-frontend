6772fd7f7124416b0a6a4216ca4ca2e5357b9f50
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-30 19:20:54 -0300

    Added dispatch for get info about organization

--- src/containers/Organization/OrganizationContainer.js
+++ src/containers/Organization/OrganizationContainer.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Organization from '../../components/Organization/Organization'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+	fetchOrganization,
+	selectorOrganization
+} from '../../redux/slices/organization-slice'
+
 const OrganizationContainer = () => {
+	const dispatch = useDispatch()
+	const { organization, errors } = useSelector(selectorOrganization)
+
+	useEffect(() => {
+		dispatch(fetchOrganization())
+	}, [dispatch])
 	return (
 		<>
 			<div className='col py-3'>
 				<p>This is container for organization</p>
-				<Organization></Organization>
+				<Organization organization={organization} />
 			</div>
 		</>
 	)
--- src/containers/Organization/OrganizationFormContainer.js
+++ src/containers/Organization/OrganizationFormContainer.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Organization from '../../components/Organization/Organization'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+	fetchOrganization,
+	selectorOrganization,
+	editOrganization
+} from '../../redux/slices/organization-slice'
+
 import OrganizationForm from '../../components/Organization/OrganizationForm'
+
 const OrganizationFormContainer = () => {
+	const dispatch = useDispatch()
+	const { errors, organization } = useSelector(selectorOrganization)
+
+	const handleSumbmit = (data) => {
+		dispatch(editOrganization(data))
+	}
 	return (
-		<div>
+		<div className='col'>
 			<p>OrganizationFormContainer</p>
-			<OrganizationForm></OrganizationForm>
+			<OrganizationForm
+				handleSumbmit={handleSumbmit}
+				organization={organization}
+				errors={errors}
+			/>
 		</div>
 	)
 }
