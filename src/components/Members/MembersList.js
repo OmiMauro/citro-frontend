1608a13566a14068565fc2d951e9641d59b92776
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-05 11:24:53 -0300

    Dispatch for remove a organizator

--- src/components/Members/MembersList.js
+++ src/components/Members/MembersList.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-const MembersList = ({ members, onDelete }) => {
+const MembersList = ({ members, handleDelete }) => {
+	const onDelete = (id) => {
+		handleDelete(id)
+	}
 	return (
 		<>
 			<div className='container '>
@@ -22,57 +25,57 @@ const MembersList = ({ members, onDelete }) => {
 								key={member._id} /* style={{ width: '18rem' }} */
 							>
 								<img
-									src={member.image_id.url}
+									src={member?.image_id?.url}
 									className='card-img-top m-2 w-25'
-									alt={member.text}
+									alt={member?.text}
 								/>
 								<div className='card-title'>
 									<h6 className=' mb-2 text-muted h4'>
-										{member.name}, {member.lastname}
+										{member?.name}, {member?.lastname}
 									</h6>
 								</div>
 								<div className='card-body '>
 									<div className='d-flex justify-content-center'>
 										<a
 											className='card-subtitle mb-2 text-muted h-6 text-black'
-											href={member.urlFacebook}
+											href={member?.urlFacebook}
 											target={'_blank'}>
-											{member.urlFacebook}
+											{member?.urlFacebook}
 										</a>
 									</div>
 									<div className='d-flex justify-content-center'>
 										<a
 											className='card-subtitle mb-2 text-muted h-6 text-black'
-											href={member.urlInstagram}
+											href={member?.urlInstagram}
 											target={'_blank'}>
-											{member.urlInstagram}
+											{member?.urlInstagram}
 										</a>
 									</div>
 									<div className='d-flex justify-content-center'>
 										<a
 											className='card-subtitle mb-2 text-muted h-6 text-black'
-											href={member.urlWhatsapp}
+											href={member?.urlWhatsapp}
 											target={'_blank'}>
-											{member.urlWhatsapp}
+											{member?.urlWhatsapp}
 										</a>
 									</div>
 									<div className='d-flex justify-content-center'>
 										<a
 											className='card-subtitle mb-2 text-muted h-6 text-black'
-											href={member.phone}
+											href={member?.phone}
 											target={'_blank'}>
-											{member.phone}
+											{member?.phone}
 										</a>
 									</div>
-									<div class='card-footer text-muted justify-content-center d-flex'>
+									<div className='card-footer text-muted justify-content-center d-flex'>
 										<Link
-											to={`/backoffice/members/edit/${member._id}`}
+											to={`/backoffice/members/edit/${member?._id}`}
 											className=' btn btn-outline-dark'>
 											Editar Organizador
 										</Link>
 										<button
-											onClick={onDelete}
-											className=' btn  btn-outline-dark'>
+											onClick={() => onDelete(member._id)}
+											className=' btn btn-outline-dark'>
 											Eliminar Organizador
 										</button>
 									</div>
@@ -80,63 +83,6 @@ const MembersList = ({ members, onDelete }) => {
 							</div>
 						)
 					})}
-					{/* <table className='table align-middle table-hover table-striped table-list table-bordered  table-sm'>
-						<thead className='thead-list'>
-							<tr>
-								<th>Nombre</th>
-								<th>Apellido</th>
-								<th>Facebook</th>
-								<th>Instagram</th>
-								<th>Whatsapp</th>
-								<th>Foto</th>
-								<th>Acciones</th>
-							</tr>
-						</thead>
-						<tbody>
-							{members.map((member) => {
-								return (
-									<tr key={member._id}>
-										<td className='align-middle'>
-											<p className='mt-3'>{member.name}</p>
-										</td>
-										<td className='align-middle'>
-											<p className='mt-3'>{member.lastname}</p>
-										</td>
-										<td className='align-middle'>
-											<a className='mt-3 text-black'>{member.urlFacebook}</a>
-										</td>
-										<td className='align-middle'>
-											<a className='mt-3 text-black'>{member.urlInstagram}</a>
-										</td>
-										<td className='align-middle'>
-											<a className='mt-3 text-black'>{member.urlWhatsapp}</a>
-										</td>
-										<td className='align-middle'>
-											<img
-												alt='member'
-												width={'40px'}
-												className='img-table rounded mx-auto d-block '
-												src={member.image_id.url}
-											/>
-										</td>
-										<td className='align-middle'>
-											<Link to={`/backoffice/members/edit/${member._id}`}>
-												<button className='btn-list btn-edit'>
-													<i className='fas fa-pencil-alt' />
-												</button>
-											</Link>
-											<button
-												className='btn-list btn-delete'
-												title='Eliminar'
-												onClick={() => onDelete(member._id)}>
-												<i className='fa fa-trash-alt' />
-											</button>
-										</td>
-									</tr>
-								)
-							})}
-						</tbody>
-					</table> */}
 				</div>
 			</div>
 		</>
--- src/containers/Members/MembersListContainer.js
+++ src/containers/Members/MembersListContainer.js
@@ -1,21 +1,31 @@
 import React, { useEffect } from 'react'
 import MembersList from '../../components/Members/MembersList'
 import { useDispatch, useSelector } from 'react-redux'
-import { selectorMembers, fetchMembers } from '../../redux/slices/members-slice'
+import {
+	selectorMembers,
+	fetchMembers,
+	removeMember
+} from '../../redux/slices/members-slice'
 
 const MembersListContainer = () => {
 	const { members, status, errors } = useSelector(selectorMembers)
 	const dispatch = useDispatch()
+
 	useEffect(() => {
 		dispatch(fetchMembers())
 	}, [dispatch])
 
+	const handleDelete = (id) => {
+		if (id) {
+			dispatch(removeMember(id))
+		}
+	}
 	return (
 		<div className='col'>
 			<h3 className='text-center fw-bold mb-5 mx-1 mx-md-4 mt-4'>
 				Lista de los organizadores
 			</h3>
-			<MembersList members={members} />
+			<MembersList members={members} handleDelete={handleDelete} />
 		</div>
 	)
 }
