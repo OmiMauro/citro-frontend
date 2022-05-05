e72798a6b6784dae83034b15e13f1ef72eb8c40f
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-05 15:20:35 -0300

    Table for view all users register in database

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,7 +1,95 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const UsersList = () => {
-	return <div>UsersList</div>
+const onDelete = (e) => {}
+const UsersList = ({ users = [] }) => {
+	return (
+		<>
+			<div className='col'>
+				<div className='container'>
+					<h3 className='text-center fw-bold mb-5 mx-1 mx-md-4 mt-4'>
+						Lista de Usuarios
+					</h3>
+					<Link
+						to={`/backoffice/slides/create/`}
+						className='btn btn-outline-success'>
+						<i className='fas fa-pencil-alt' />
+						Lista de usuarios registrados
+					</Link>
+					<Link to={`/backoffice/`} className='btn btn-outline-success'>
+						<i className='fas fa-pencil-alt' />
+						Volver
+					</Link>
+					<div className='table-responsive'>
+						<table className='table align-middle table-hover table-striped table-list table-bordered  table-sm'>
+							<thead className='thead-list'>
+								<tr>
+									<th>Nombre</th>
+									<th>Apellido</th>
+									<th>Email</th>
+									<th>Celular</th>
+									<th>Rol</th>
+									<th>Verificado</th>
+									<th>Acciones</th>
+								</tr>
+							</thead>
+							<tbody>
+								{users?.map((user) => {
+									return (
+										<tr key={user._id}>
+											<td className='align-middle'>
+												<p className='mt-3'>{user.name}</p>
+											</td>
+											<td className='align-middle'>
+												<p className='mt-3'>{user.lastname}</p>
+											</td>
+											<td className='align-middle'>
+												<p className='mt-3'>{user.email}</p>
+											</td>
+											<td className='align-middle'>
+												<p className='mt-3'>{user.phone}</p>
+											</td>
+											<td className='align-middle'>
+												<p className='mt-3'>{user.roleId}</p>
+											</td>
+											<td className='align-middle'>
+												<p className='mt-3'>{user.isVerified ? 'Si' : 'No'}</p>
+											</td>
+											{/* <td className='align-middle'>
+											<img
+												alt='user'
+												width={'40px'}
+												className='img-table rounded mx-auto d-block '
+												src={user.image_id.url}
+											/>
+										</td> */}
+											<td className='align-middle'>
+												<Link
+													to={`/backoffice/users/edit/${user._id}`}
+													className='mt-1'>
+													<button className='btn-list btn-edit'>
+														Edit
+														<i className='fas fa-pencil-alt' />
+													</button>
+												</Link>
+												<button
+													className='btn-list btn-delete'
+													title='Eliminar'
+													onClick={() => onDelete(user._id)}>
+													Dels
+													<i className='fa fa-trash-alt' />
+												</button>
+											</td>
+										</tr>
+									)
+								})}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default UsersList
--- src/containers/Users/UsersListContainer.js
+++ src/containers/Users/UsersListContainer.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import UsersList from '../../components/Users/UsersList'
+import { selectorUsers, fetchUsers } from '../../redux/slices/users-slice'
+
 const UsersListContainer = () => {
+	const dispatch = useDispatch()
+	const { users, errors } = useSelector(selectorUsers)
+
+	useEffect(() => {
+		dispatch(fetchUsers(null))
+	}, [dispatch])
+
 	return (
 		<>
-			<p>UsersListContainer</p>
-			<UsersList />
+			<UsersList users={users} errors={errors} />
 		</>
 	)
 }
