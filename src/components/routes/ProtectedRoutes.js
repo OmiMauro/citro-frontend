c195619df4bea65cda01884d47dd605d7ea77b90
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-05 10:33:36 -0300

    Dispatch in useEffect for logout credentials from localStorage of user when expired token

--- src/components/routes/ProtectedRoutes.js
+++ src/components/routes/ProtectedRoutes.js
@@ -1,11 +1,25 @@
 import { ROLES } from '../../config'
-import { selectorAuth } from '../../redux/slices/auth-slice'
-import { useSelector } from 'react-redux'
+import { selectorAuth, isAuth, logout } from '../../redux/slices/auth-slice'
+import { useDispatch, useSelector } from 'react-redux'
 import { Outlet, Navigate, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
+const parseToken = (token) => {
+	try {
+		return JSON.parse(atob(token?.split('.')[1]))
+	} catch (error) {
+		return null
+	}
+}
 const ProtectedRoutes = ({ allowedRoles }) => {
-	const { auth, user } = useSelector(selectorAuth)
-	//	const location = useLocation()
+	const { auth, token, user } = useSelector(selectorAuth)
+	const dispatch = useDispatch()
+	useEffect(() => {
+		const decodedToken = parseToken(token)
+		if (decodedToken.exp * 1000 < Date.now()) {
+			dispatch(logout())
+		}
+	}, [auth])
 	return auth ? (
 		allowedRoles?.includes(user.roleId) ? (
 			<Outlet />
