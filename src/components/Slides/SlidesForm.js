ac051e1bbca5f95e1817505b9f6c5c99b57c9cae
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-05 15:23:22 -0300

    Form for create or update a slide

--- src/components/Slides/SlidesForm.js
+++ src/components/Slides/SlidesForm.js
@@ -1,7 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SlidesForm = () => {
-	return <div>SlidesForm</div>
+const SlidesForm = ({ slide = {}, handleSubmit, errors }) => {
+	const [values, setValues] = useState({
+		image: slide.image ?? '',
+		text: slide.text ?? '',
+		order: slide.order ?? ''
+	})
+	const { image, text, order } = values
+	const handleChange = (e) => {
+		const { name, value } = e.target
+		if (name === 'image') setValues({ ...values, name: e.target.files[0] })
+		setValues({ ...values, [name]: value })
+	}
+	const onSubmit = (e) => {
+		e.preventDefault()
+		const formData = new FormData(e.currentTarget)
+		handleSubmit(formData)
+	}
+	return (
+		<>
+			<div className='container'>
+				<div className='row d-flex justify-content-center align-items-center '>
+					<h3>Agregar un nuevo slide </h3>
+				</div>
+				<div className='row d-flex justify-content-center align-items-center '>
+					<div className='col-lg-12 col-xl-11'>
+						<div className='row justify-content-start'>
+							<div className='col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1'>
+								<form className='mx-1 mx-md-4' onSubmit={onSubmit}>
+									<div className='d-flex flex-row align-items-center mb-4'>
+										<i className='fa fa-long-arrow-right fa-lg me-3 fa-fw'></i>
+										<div className='form-outline flex-fill mb-0'>
+											<input
+												className='form-control'
+												id='image'
+												onChange={handleChange}
+												value={image}
+												type='file'
+												placeholder='image'
+												name='image'
+											/>
+											<label htmlFor='image' className='form-label'>
+												Imagen*
+											</label>
+											{errors?.map(
+												(err) =>
+													err.param == 'image' && (
+														<div className='text-danger'>{err.msg}</div>
+													)
+											)}
+										</div>
+									</div>
+									<div className='d-flex flex-row align-items-center mb-4'>
+										<i className='fa fa-long-arrow-right fa-lg me-3 fa-fw'></i>
+										<div className='form-outline flex-fill mb-0'>
+											<input
+												className='form-control'
+												onChange={handleChange}
+												id='text'
+												type='text'
+												placeholder='Descripcion'
+												value={text}
+												name='text'
+											/>
+											<label htmlFor='text' className='form-label'>
+												Descripción
+											</label>
+											{errors?.map(
+												(err) =>
+													err.param == 'text' && (
+														<div className='text-danger'>{err.msg}</div>
+													)
+											)}
+										</div>
+									</div>
+									<div className='d-flex justify-content-center mx-4 mb-3 mb-lg-4'>
+										<button type='submit' className='btn btn-primary btn-lg'>
+											Crear
+										</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default SlidesForm
--- src/containers/Slides/SlidesFormContainer.js
+++ src/containers/Slides/SlidesFormContainer.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SlidesForm from '../../components/Slides/SlidesForm'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectorSlides, fetchSlideById } from '../../redux/slices/slides-slice'
+
 const SlidesFormContainer = () => {
+	const handleSubmit = (formData) => {}
+	const { id } = useParams()
+	const dispatch = useDispatch()
+	const { slide, errors, loading } = useSelector(selectorSlides())
+
+	useEffect(() => {
+		if (id) {
+			dispatch(fetchSlideById(id))
+		}
+	}, [dispatch])
 	return (
-		<div>
-			<p>SlidesFormContainer</p>
-			<SlidesForm></SlidesForm>
-		</div>
+		<>
+			<SlidesForm handleSubmit={handleSubmit} slide={slide} />
+		</>
 	)
 }
 
