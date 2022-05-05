ae8096a87d34269aaba12022cd25727cdc1b42ac
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-05 15:22:52 -0300

    Component for view all slides the organization in /backoffice/slides

--- src/components/Slides/SlidesList.js
+++ src/components/Slides/SlidesList.js
@@ -4,89 +4,49 @@ const SlidesList = ({ slides = [], handleDelete }) => {
 	const onDelete = () => {}
 	return (
 		<>
-			<div className='container'>
-				<Link
-					to={`/backoffice/slides/create/`}
-					className='btn btn-outline-success'>
-					<i className='fas fa-pencil-alt' />
-					Crear Nuevo Slide
-				</Link>
-				<Link to={`/backoffice/`} className='btn btn-outline-success'>
-					<i className='fas fa-pencil-alt' />
-					Volver
-				</Link>
-				<div className='row mt-4'>
-					{slides?.map((slide) => {
-						return (
-							<div className='card col-12 m-3' /* style={{ width: '18rem' }} */>
-								<img
-									src={slide.image_id.url}
-									className='card-img-top m-2'
-									alt={slide.text}
-								/>
-
-								<div className='card-body'>
-									<h6 className='card-subtitle mb-2 text-muted'>
-										Orden: {slide.order}
-									</h6>
-
-									<Link
-										to={`/backoffice/slides/edit/${slide._id}`}
-										className=' btn btn-outline-dark'>
-										Editar Slide
-									</Link>
-									<button onClick={onDelete} className=' btn  btn-outline-dark'>
-										Eliminar Slide
-									</button>
-								</div>
-							</div>
-						)
-					})}
-					{/* 	<table className='table align-middle table-hover table-striped table-list table-bordered  table-sm'>
-					<thead className='thead-list'>
-					<tr>
-					<th>Texto</th>
-					<th>Order</th>
-					<th>Image</th>
-					</tr>
-					</thead>
-					<tbody>
-						{slides.map((slide) => {
+			<div className='col'>
+				<h3 className='text-center fw-bold mb-5 mx-1 mx-md-4 mt-4'>
+					Lista de Slides
+				</h3>
+				<div className='container'>
+					<Link
+						to={`/backoffice/slides/create/`}
+						className='btn btn-outline-success'>
+						<i className='fas fa-pencil-alt' />
+						Crear Nuevo Slide
+					</Link>
+					<Link to={`/backoffice/`} className='btn btn-outline-success'>
+						<i className='fas fa-pencil-alt' />
+						Volver
+					</Link>
+					<div className='row mt-4'>
+						{slides?.map((slide) => {
 							return (
-								<tr key={slide._id}>
-								<td className='align-middle'>
-								<p className='mt-3'>{slide.text}</p>
-								</td>
-								<td className='align-middle'>
-								<p className='mt-3'>{slide.order}</p>
-								</td>
-								
-									<td className='align-middle'>
+								<div className='card col-12 m-3'>
 									<img
-									alt='slide'
-									width={'40px'}
-									className='img-table rounded mx-auto d-block '
-									src={slide.image_id.url}
-										/>
-										</td>
-										<td className='align-middle'>
-										<Link to={`/backoffice/slides/edit/${slide._id}`}>
-										<button className='btn-list btn-edit'>
-										<i className='fas fa-pencil-alt' />
-										</button>
+										src={slide.image_id.url}
+										className='card-img-top m-2'
+										alt={slide.text}
+									/>
+
+									<div className='card-body'>
+										<h6 className='card-subtitle mb-2 text-muted'>
+											Orden: {slide.order}
+										</h6>
+
+										<Link
+											to={`/backoffice/slides/edit/${slide._id}`}
+											className=' btn btn-outline-dark'>
+											Editar Slide
 										</Link>
-										<button
-											className='btn-list btn-delete'
-											title='Eliminar'
-											onClick={() => onDelete(slide._id)}>
-											<i className='fa fa-trash-alt' />
+										<button onClick={onDelete} className='btn btn-outline-dark'>
+											Eliminar Slide
 										</button>
-										</td>
-										</tr>
-										)
-									})}
-									</tbody>
-								</table> */}
+									</div>
+								</div>
+							)
+						})}
+					</div>
 				</div>
 			</div>
 		</>
--- src/containers/Slides/SlidesListContainer.js
+++ src/containers/Slides/SlidesListContainer.js
@@ -7,20 +7,14 @@ import { useDispatch, useSelector } from 'react-redux'
 const SlidesListContainer = () => {
 	const { slides, status, errors } = useSelector(selectorSlides)
 	const dispatch = useDispatch()
-
+	const handleDelete = (id) => {}
 	useEffect(() => {
 		dispatch(fetchSlides())
 	}, [dispatch])
 
-	const handleDelete = () => {}
 	return (
 		<>
-			<div className='col'>
-				<h3 className='text-center fw-bold mb-5 mx-1 mx-md-4 mt-4'>
-					Lista de Slides
-				</h3>
-				<SlidesList slides={slides} handleDelete={handleDelete} />
-			</div>
+			<SlidesList slides={slides} handleDelete={handleDelete} />
 		</>
 	)
 }
