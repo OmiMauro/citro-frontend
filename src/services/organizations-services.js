11d73d8800e766c0fd25d6f69534fd2b991aad42
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-24 19:33:04 -0300

    Refactor: Pass how param ID of fetch organization

--- src/services/organizations-services.js
+++ src/services/organizations-services.js
@@ -1,10 +1,12 @@
 import { get } from './public-api-services'
 import { put } from './private-api-services'
 const ENDPOINT = process.env.REACT_APP_API_ORGANIZATIONS_ENDPOINT
+const ID_ORGANIZATION = process.env.REACT_APP_API_ID_ORGANIZATION
+
 export const getOrganization = async (search = null) => {
-	return await get(ENDPOINT)
+	return await get(ENDPOINT, {}, ID_ORGANIZATION)
 }
 
 export const putOrganization = async (data) => {
-	return await put(ENDPOINT, data)
+	return await put(ENDPOINT, data, ID_ORGANIZATION)
 }
