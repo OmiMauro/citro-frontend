b76067f0e4133d8ce9dc6e166ae8502b3262b85f
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-24 19:30:34 -0300

    Added configs for ckeditor5

---
+++ src/components/Editor/editorConfig.js
@@ -0,0 +1,20 @@
+// editorConfig.js
+
+export const editorConfig = {
+	toolbar: {
+		items: [
+			'bold',
+			'italic',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'indent',
+			'outdent',
+			'|',
+			'heading',
+			'|',
+			'undo',
+			'redo'
+		]
+	}
+}
