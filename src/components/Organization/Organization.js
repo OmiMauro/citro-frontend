24a8af2150b3fa171e7f60ec7b77b1e3abb6b363
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-29 11:40:53 -0300

    Styles: refactor component for view data of organization in dashboard

--- src/components/Organization/Organization.js
+++ src/components/Organization/Organization.js
@@ -1,130 +1,106 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './styles.css'
 import DangerousSetInnerHTML from '../DangerousSetInnerHTML/DangerousSetInnerHTML'
 const Organization = ({ organization }) => {
 	return (
 		<>
-			<section className='page-section' id='services'>
-				<div className='container' key={organization._id}>
-					<div className='text-center'>
-						<h2 className='section-heading text-uppercase text-center h1'>
-							{organization?.name}
-						</h2>
-						<h3 className='section-subheading text-muted h1'>
-							{organization?.welcomeText}
-						</h3>
+			<section className='col' key={organization._id}>
+				<div class='card'>
+					<div class='card__img'>
+						<img className='img' src={organization?.image_id?.url} />
 					</div>
-					<div className='d-flex justify-content-center'>
-						<img
-							alt={organization?.name}
-							className='img w-50'
-							src={organization?.image}
-						/>
+					<div class='card__name'>{organization?.name}</div>
+					<div class='card__job m-5'>
+						<span className='h5'>Texto de bienvenida:</span>
+						<hr className='w-25 mx-auto' />
+						<span>{organization?.welcomeText}</span>
 					</div>
-
-					<div className='row text-center text-muted'>
+					<div className='card__job mt-4'>
+						<span className='h5'>Sobre Nosotros:</span>
+						<hr className='w-25 mx-auto' />
 						<DangerousSetInnerHTML content={organization?.aboutUs} />
 					</div>
-
-					<div className='mx-auto m-3'>
-						<h3 className='text-center text-uppercase'>Redes Sociales</h3>
-						<div className='row'>
-							<div className='col-xl-3 col-md-6'>
-								<div className='card bg-success text-white mb-4'>
-									<p className='card-body'>Celular</p>
-									<div className='card-footer d-flex align-items-center justify-content-between'>
-										{organization?.phone ? (
-											<a
-												className='small text-white stretched-link'
-												href={`tel:${organization?.phone}`}>
-												{organization?.phone}
-											</a>
-										) : (
-											<p>No has ingresado el celular</p>
-										)}
-									</div>
-								</div>
-							</div>
-							<div className='col-xl-3 col-md-6'>
-								<div className='card bg-success text-white mb-4'>
-									<p className='card-body'>Correo Electronico</p>
-									<div className='card-footer d-flex align-items-center justify-content-between'>
-										{organization?.email ? (
-											<a
-												className='small text-white stretched-link'
-												href={organization?.email}
-												target={'_blank'}>
-												{organization?.email}
-											</a>
-										) : (
-											<p>No has ingresado el email</p>
-										)}
-									</div>
-								</div>
-							</div>
-							<div className='col-xl-3 col-md-6'>
-								<div className='card bg-success text-white mb-4'>
-									<p className='card-body'>Facebook</p>
-									<div className='card-footer d-flex align-items-center justify-content-between'>
-										{organization?.urlFacebook ? (
-											<a
-												className='small text-white stretched-link'
-												href={organization?.urlFacebook}
-												target={'_blank'}>
-												{organization?.urlFacebook}
-											</a>
-										) : (
-											<p>No has ingresado la url</p>
-										)}
-									</div>
-								</div>
-							</div>
-							<div className='col-xl-3 col-md-6'>
-								<div className='card bg-success text-white mb-4'>
-									<p className='card-body'>Instagram</p>
-									<div className='card-footer d-flex align-items-center justify-content-between'>
-										{organization?.urlInstagram ? (
-											<a
-												className='small text-white stretched-link'
-												href={organization?.urlInstagram}
-												target={'_blank'}>
-												{organization?.urlInstagram}
-											</a>
-										) : (
-											<p>No has ingresado la url</p>
-										)}
-									</div>
-								</div>
-							</div>
-							<div className='col-xl-3 col-md-6'>
-								<div className='card bg-success text-white mb-4'>
-									<p className='card-body'>Whatsapp</p>
-									<div className='card-footer d-flex align-items-center justify-content-between'>
-										{organization?.urlWhatsapp ? (
-											<a
-												className='small text-white stretched-link'
-												href={organization?.urlWhatsapp}
-												target={'_blank'}>
-												{organization?.urlWhatsapp}
-											</a>
-										) : (
-											<p>No has ingresado la url</p>
-										)}
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div className='d-flex justify-content-start mt-4'>
-							<Link
-								className='btn btn-outline-primary'
-								to={`/backoffice/organizations/edit/${1}`}>
-								Editar
-							</Link>
-							<Link className='btn btn-outline-primary' to={'/backoffice'}>
-								Volver
-							</Link>
-						</div>
+					<div class='card__link'>
+						<span className='h5'>Enlaces</span>
+						<hr className='w-25 mx-auto' />
+						<article>
+							Celular:
+							{organization?.phone ? (
+								<a
+									className='small  stretched-link'
+									href={`tel:${organization?.phone}`}>
+									<i className='fa fa-phone' aria-hidden='true'></i>
+								</a>
+							) : (
+								<span> No has ingresado el celular</span>
+							)}
+						</article>
+						<article>
+							Email:
+							{organization?.email ? (
+								<a
+									className='small  stretched-link'
+									href={`mailto:${organization?.email}`}
+									target={'_blank'}>
+									<i className='fa fa-at' aria-hidden='true'></i>
+								</a>
+							) : (
+								<span> No has ingresado el email</span>
+							)}
+						</article>
+						<article>
+							Facebook:
+							{organization?.urlFacebook ? (
+								<a
+									className='small  stretched-link'
+									href={organization?.urlFacebook}
+									target={'_blank'}>
+									<i className='fa fa-facebook' aria-hidden='true'></i>
+								</a>
+							) : (
+								<span> No has ingresado la url</span>
+							)}
+						</article>
+						<article>
+							Instagram:
+							{organization?.urlInstagram ? (
+								<a
+									className='small  stretched-link'
+									href={organization?.urlInstagram}
+									target={'_blank'}>
+									<i className='fa fa-instagram' aria-hidden='true'></i>
+								</a>
+							) : (
+								<span> No has ingresado la url</span>
+							)}
+						</article>{' '}
+						<article>
+							Whatsapp:
+							{organization?.urlWhatsapp ? (
+								<a
+									className='small  stretched-link'
+									href={organization?.urlWhatsapp}
+									target={'_blank'}>
+									<i className='fa fa-whatsapp' aria-hidden='true'></i>
+								</a>
+							) : (
+								<span> No has ingresado la url</span>
+							)}
+						</article>
+					</div>
+					<hr className='w-25 mx-auto' />
+					<div className=''>
+						<Link
+							className=' card__btn-contact btn btn-outline-primary'
+							to={`/backoffice/organizations/edit/${1}`}>
+							Editar
+						</Link>
+						<Link
+							className='btn btn-outline-primary card__btn-contact'
+							to={'/backoffice'}>
+							Volver
+						</Link>
 					</div>
 				</div>
 			</section>
