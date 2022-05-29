988a9c0c30235f6605a95bdcea87539d8a4da66a
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-29 16:06:43 -0300

    Add DangerousSetHTML in description AboutUs

--- src/components/About/AboutUs.js
+++ src/components/About/AboutUs.js
@@ -7,15 +7,14 @@ import { useEffect } from 'react'
 
 import './styles.css'
 import { STATUS } from '../../redux/constants/action-types'
+import DangerousSetInnerHTML from '../DangerousSetInnerHTML/DangerousSetInnerHTML'
 const AboutUs = () => {
 	const { organization, status } = useSelector(selectorOrganization)
 	const dispatch = useDispatch()
 	useEffect(() => {
 		dispatch(fetchOrganization())
 	}, [dispatch])
-	const splitedDescription = organization?.aboutUs
-		? organization?.aboutUs.split('. ')
-		: ['']
+
 	return (
 		<section className='page-section' id=''>
 			<div className='container'>
@@ -29,15 +28,13 @@ const AboutUs = () => {
 				</div>
 				<div className='row d-flex justify-content-center'>
 					<img
-						src={organization.image}
+						src={organization?.image_id?.url}
 						alt={organization?.name}
 						className='col-8'
 					/>
 				</div>
 				<div className='row text-center fst-italic text-muted'>
-					{splitedDescription?.map((paragraph, index) => {
-						return <p key={index}>{paragraph}.</p>
-					})}
+					<DangerousSetInnerHTML content={organization?.aboutUs} />
 				</div>
 			</div>
 		</section>
