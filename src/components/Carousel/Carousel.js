e2cf319afc370d50cb6b87d5fc83e49a439cd8b0
Manu <ominuka.mauro@gmail.com>
2022-06-04 15:11:17 -0300

    Added key_id

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,45 +1,48 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { selectorSlides, fetchSlides } from '../../redux/slices/slides-slice'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import 'bootstrap/js/dist/carousel'
 import { STATUS } from '../../redux/constants/action-types'
-//import './styles.css'
+// import './styles.css'
 
 const Carousel = () => {
-	const dispatch = useDispatch()
-	const { slides, status } = useSelector(selectorSlides)
+  const dispatch = useDispatch()
+  const { slides, status } = useSelector(selectorSlides)
 
-	useEffect(() => {
-		dispatch(fetchSlides())
-	}, [])
+  useEffect(() => {
+    dispatch(fetchSlides())
+  }, [])
 
-	return (
-		<>
-			{status === STATUS.SUCCESSFUL && (
-				<div className='carousel slide' data-bs-ride='carousel'>
-					<div className='carousel-inner'>
-						{slides?.map((item, index) => (
-							<div
-								className={`carousel-item ${index === 0 ? 'active' : ''}`}
-								key={item._id}>
-								<img
-									className='d-block w-100'
-									src={item.image_id?.url}
-									alt={item.text}
-									key={item._id}
-								/>
-							</div>
-						))}
-					</div>
-				</div>
-			)}
-			{status === STATUS.PENDING && (
-				<div class='spinner-border text-secondary' role='status'>
-					<span class='visually-hidden'>Loading...</span>
-				</div>
-			)}
-		</>
-	)
+  return (
+    <>
+      {status === STATUS.SUCCESSFUL && (
+        <div className='carousel slide' data-bs-ride='carousel'>
+          <div className='carousel-inner'>
+            {slides?.map((item, index) => (
+              <div
+                className={`carousel-item ${index === 0 ? 'active' : ''}`}
+                key={item._id}
+              >
+                <img
+                  className='d-block w-100'
+                  src={item.image_id?.url}
+                  alt={item.text}
+                  key={item._id}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      {status === STATUS.PENDING && (
+        <div className='d-flex justify-content-center '>
+          <div className='spinner-border text-secondary' role='status'>
+            <span className='visually-hidden'>Loading...</span>
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Carousel
