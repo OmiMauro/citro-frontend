b6795ede333e17e4cc7ba162db98e1dfe5893868
Manu <ominuka.mauro@gmail.com>
2022-06-08 21:03:40 -0300

    Style: change single quote by double

--- src/components/Backoffice/SideNav/SideNav.js
+++ src/components/Backoffice/SideNav/SideNav.js
@@ -10,146 +10,160 @@ const SideNav = () => {
 	const { user: userId } = useSelector(selectorUsers)
 	const dispatch = useDispatch()
 	return (
-		<div className='col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark'>
-			<div className='d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100'>
+		<div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+			<div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
 				<Link
-					to='/'
-					className='d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none'>
-					<span className='fs-5 d-none d-sm-inline'>Inicio</span>
+					to="/"
+					className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+				>
+					<span className="fs-5 d-none d-sm-inline">Inicio</span>
 				</Link>
 				{user.roleId === 2 && (
 					<ul
-						className='nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start'
-						id='menu'>
+						className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+						id="menu"
+					>
 						<li>
-							<Link to='/backoffice' className='nav-link align-middle px-0'>
-								<i className='fs-4 bi-table fa fa-table'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Home</span>
+							<Link to="/backoffice" className="nav-link align-middle px-0">
+								<i className="fs-4 bi-table fa fa-table"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Home</span>
 							</Link>
 						</li>
 						<li>
 							<Link
-								to='/backoffice/organizations'
-								className='nav-link align-middle px-0'>
-								<i className='fs-4 bi-table fa fa-building'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Organizations</span>
+								to="/backoffice/organizations"
+								className="nav-link align-middle px-0"
+							>
+								<i className="fs-4 bi-table fa fa-building"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Organizations</span>
 							</Link>
 						</li>
 						<li>
 							<Link
-								to='/backoffice/profile'
-								className='nav-link px-0 align-middle '>
-								<i className='fs-4 fa fa-user-circle'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Profile</span>
+								to="/backoffice/profile"
+								className="nav-link px-0 align-middle "
+							>
+								<i className="fs-4 fa fa-user-circle"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Profile</span>
 							</Link>
 						</li>
 						<li>
 							<Link
-								to='/backoffice/members'
-								className='nav-link align-middle px-0'>
-								<i className='fs-4  fa fa-users'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Organizadores</span>
+								to="/backoffice/members"
+								className="nav-link align-middle px-0"
+							>
+								<i className="fs-4  fa fa-users"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Organizadores</span>
 							</Link>
 						</li>
 						<li>
 							<Link
-								to='/backoffice/slides'
-								className='nav-link align-middle px-0'>
-								<i className='fs-4 fa fa-image'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Slides</span>
+								to="/backoffice/slides"
+								className="nav-link align-middle px-0"
+							>
+								<i className="fs-4 fa fa-image"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Slides</span>
 							</Link>
 						</li>
 						<li>
 							<Link
-								to='/backoffice/galery'
-								className='nav-link align-middle px-0'>
-								<i className='fs-4 fa fa-camera'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Galería</span>
+								to="/backoffice/galery"
+								className="nav-link align-middle px-0"
+							>
+								<i className="fs-4 fa fa-camera"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Galería</span>
 							</Link>
 						</li>
 						<li>
 							<Link
-								to='/backoffice/users'
-								className='nav-link align-middle px-0'>
-								<i className='fs-4 fa fa-user'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Usuarios</span>
+								to="/backoffice/users"
+								className="nav-link align-middle px-0"
+							>
+								<i className="fs-4 fa fa-user"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Usuarios</span>
 							</Link>
 						</li>
 						<li>
 							<Link
-								to='/backoffice/events'
-								className='nav-link align-middle px-0'>
-								<i className='fs-4 fa fa-calendar'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Eventos</span>
+								to="/backoffice/events"
+								className="nav-link align-middle px-0"
+							>
+								<i className="fs-4 fa fa-calendar"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Eventos</span>
 							</Link>
 						</li>
 					</ul>
 				)}
 				{user.roleId === 1 && (
 					<ul
-						className='nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start'
-						id='menu'>
+						className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+						id="menu"
+					>
 						<li>
-							<Link to='/backoffice' className='nav-link align-middle px-0'>
-								<i className='fs-4 fa fa-table'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Home</span>
+							<Link to="/backoffice" className="nav-link align-middle px-0">
+								<i className="fs-4 fa fa-table"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Home</span>
 							</Link>
 						</li>
 
 						<li>
 							<Link
-								to='/backoffice/profile'
-								className='nav-link px-0 align-middle '>
-								<i className='fs-4 fa fa-user-circle'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Profile</span>
+								to="/backoffice/profile"
+								className="nav-link px-0 align-middle "
+							>
+								<i className="fs-4 fa fa-user-circle"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Profile</span>
 							</Link>
 						</li>
 
 						<li>
 							<Link
-								to='/backoffice/events'
-								className='nav-link align-middle px-0'>
-								<i className='fs-4 fa fa-calendar'></i>{' '}
-								<span className='ms-1 d-none d-sm-inline'>Eventos</span>
+								to="/backoffice/events"
+								className="nav-link align-middle px-0"
+							>
+								<i className="fs-4 fa fa-calendar"></i>{' '}
+								<span className="ms-1 d-none d-sm-inline">Eventos</span>
 							</Link>
 						</li>
 					</ul>
 				)}
 				<hr />
-				<div className='fixed-bottom dropdown p-4'>
+				<div className=" dropdown p-4">
 					<Link
-						to='profile/:id'
-						className='d-flex align-items-center text-white text-decoration-none dropdown-toggle'
-						id='dropdownUser1'
-						data-bs-toggle='dropdown'
-						aria-expanded='false'>
+						to={`profile/${userId._id}`}
+						className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+						id="dropdownUser1"
+						data-bs-toggle="dropdown"
+						aria-expanded="false"
+					>
 						<img
 							src={
 								userId?.image_id?.url ||
 								'https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg'
 							}
-							alt='avatar'
-							width='30'
-							height='30'
-							className='rounded-circle'
+							alt="avatar"
+							width="30"
+							height="30"
+							className="rounded-circle"
 						/>
-						<span className='d-none d-sm-inline mx-1'>{user.name}</span>
+						<span className="d-none d-sm-inline mx-1">{user.name}</span>
 					</Link>
-					<ul className='dropdown-menu dropdown-menu-dark text-small shadow'>
+					<ul className="dropdown-menu dropdown-menu-dark text-small shadow">
 						<li>
-							<Link to='/backoffice/profile' className='dropdown-item'>
+							<Link to="/backoffice/profile" className="dropdown-item">
 								Profile
 							</Link>
 						</li>
 						<li>
-							<hr className='dropdown-divider' />
+							<hr className="dropdown-divider" />
 						</li>
 						<li>
 							<button
-								className='dropdown-item'
+								className="dropdown-item"
 								onClick={() => {
 									dispatch(logout())
-								}}>
+								}}
+							>
 								Cerrar Sesion
 							</button>
 						</li>
