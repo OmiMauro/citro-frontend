3ffe047b3c57c3025133dcef433b5e3da28c56cc
Manu <ominuka.mauro@gmail.com>
2022-06-20 11:49:12 -0300

    Added routes for events and events/:id

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -11,7 +11,7 @@ import ProfileFormContainer from '../../containers/Profile/ProfileFormContainer'
 import ProfileContainer from '../../containers/Profile/ProfileContainer'
 import SlidesListContainer from '../../containers/Slides/SlidesListContainer'
 import SlidesFormContainer from '../../containers/Slides/SlidesFormContainer'
-
+import EventsContainer from '../../containers/Events/EventsListContainer'
 import UsersContainer from '../../containers/Users/UsersContainer'
 import UsersListContainer from '../../containers/Users/UsersListContainer'
 /* Routes Publics */
@@ -31,6 +31,9 @@ import ContactPage from '../../pages/ContactPage'
 import ForgotPasswordPage from '../../pages/ForgotPasswordPage'
 import ResetPasswordPage from '../../pages/ResetPasswordPage'
 import ConfirmEmailPage from '../../pages/ConfirmEmailPage'
+import EventsListContainer from '../../containers/Events/EventsListContainer'
+import EventsPage from '../../pages/EventsPage'
+import EventPage from '../../pages/EventPage'
 const routesPublics = [
 	{ path: '/', element: HomePage },
 	{ path: 'login', element: LoginPage },
@@ -42,140 +45,118 @@ const routesPublics = [
 	{ path: 'forgot-password', element: ForgotPasswordPage },
 	{ path: 'reset-password/:token', element: ResetPasswordPage },
 	{ path: 'confirm-email/:token', element: ConfirmEmailPage },
-	{ path: '*', element: Error404Page }
+	{ path: 'events', element: EventsPage },
+	{ path: 'events/:id', element: EventPage },
+
+	{ path: '*', element: Error404Page },
 ]
 const routesPrivates = [
 	{
 		path: '',
 		element: Backoffice,
 		allowedRoles: [ROLES.ADMIN, ROLES.CUSTOMER],
-		styles: 'nav-link align-middle px-0',
-		name: 'Home'
 	},
 
 	{
 		path: 'activities',
 		element: Backoffice,
 		allowedRoles: [ROLES.ADMIN, ROLES.CUSTOMER],
-		styles: 'nav-link align-middle px-0',
-		name: 'activities'
 	},
 	{
 		path: 'activities/create',
 		element: Backoffice,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Crear Actividad'
 	},
 	{
 		path: 'activities/edit/:id',
 		element: Backoffice,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Editar Actividad'
 	},
 	{
 		path: 'slides',
 		element: SlidesListContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'slides'
 	},
 	{
 		path: 'slides/create',
 		element: SlidesFormContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Crear Slides'
 	},
 	{
 		path: 'slides/edit/:id',
 		element: SlidesFormContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Editar Slide'
 	},
 
 	{
 		path: 'members',
 		element: MembersListContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Organizadores'
 	},
 	{
 		path: 'members/create',
 		element: MembersFormContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Crear Organizador'
 	},
 	{
 		path: 'members/edit/:id',
 		element: MembersFormContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Editar Organizador'
 	},
 	{
 		path: 'users',
 		element: UsersListContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Usuarios'
 	},
 	{
 		path: 'users/create',
 		element: UsersContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Crear usuario'
 	},
 	{
 		path: 'users/edit/:id',
 		element: UsersContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Editar Usuario'
 	},
 	{
 		path: 'organizations',
 		element: OrganizationContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: 'Organizacion'
 	},
 	{
 		path: 'organizations/edit/:id',
 		element: OrganizationFormContainer,
 		allowedRoles: [ROLES.ADMIN],
-		styles: 'nav-link align-middle px-0',
-		name: ''
 	},
 	{
 		path: 'profile',
 		element: ProfileContainer,
 		allowedRoles: [ROLES.ADMIN, ROLES.CUSTOMER],
 		styles: 'nav-link align-middle px-0',
-		name: 'Perfil'
+		name: 'Perfil',
 	},
 	{
 		path: 'profile/edit/:id',
 		element: ProfileFormContainer,
 		allowedRoles: [ROLES.ADMIN, ROLES.CUSTOMER],
 		styles: 'nav-link align-middle px-0',
-		name: 'Editar Perfil'
+		name: 'Editar Perfil',
 	},
 	{
 		path: 'galery',
 		element: GaleriesListContainer,
-		allowedRoles: [ROLES.ADMIN]
+		allowedRoles: [ROLES.ADMIN],
 	},
 	{
 		path: 'galery/create',
 		element: GaleriesContainer,
-		allowedRoles: [ROLES.ADMIN]
-	}
+		allowedRoles: [ROLES.ADMIN],
+	},
+	{
+		path: 'events',
+		element: EventsListContainer,
+		allowedRoles: [ROLES.ADMIN, ROLES.CUSTOMER],
+	},
 ]
 
 export { routesPrivates, routesPublics }
