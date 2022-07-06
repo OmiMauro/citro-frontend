36ddfcffd87c9443bf15e6db29df27db112f4b6c
Manu <ominuka.mauro@gmail.com>
2022-07-06 16:45:20 -0300

    Added react-image-lightbox in galery

--- src/components/Galery/Galery.js
+++ src/components/Galery/Galery.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { PhotoAlbum } from 'react-photo-album'
-
+import Lightbox from 'react-18-image-lightbox'
+import 'react-18-image-lightbox/style.css'
 import { fetchGalery, selectorGalery } from '../../redux/slices/galery-slice'
 import Pagination from '../Pagination/Pagination'
 import { STATUS } from '../../redux/constants/action-types'
@@ -10,12 +11,11 @@ const ImageEvent = () => {
 	const [values, setValues] = useState({
 		page: 1,
 		indexPagination: '',
-		photos: []
+		photos: [],
 	})
 	const [ligthbox, setLigthbox] = useState({
-		currentImage: '',
-		indexImage: 1,
-		openLightbox: false
+		photoIndex: 0,
+		openLightbox: false,
 	})
 
 	const { page, indexPagination, photos } = values
@@ -30,32 +30,66 @@ const ImageEvent = () => {
 	useEffect(() => {
 		setValues({
 			...values,
-			photos: galery?.docs?.map((item) => {
+			photos: galery?.docs?.map((item, index) => {
 				return {
 					src: item.image_id.secure_url,
 					width: item.image_id.width || 200,
-					height: item.image_id.height || 200
+					height: item.image_id.height || 200,
 				}
-			})
+			}),
 		})
 	}, [galery])
 
 	const handlePageChange = (e) => {
 		setValues({ ...values, page: e.selected + 1 })
 	}
-
 	return (
 		<>
-			<div className='container '>
-				<h2 className='text-center py-5 text-uppercase'>
-					Imágenes de nuestros eventos
-				</h2>
-
+			<div className="container ">
+				<h2 className="text-center py-5 text-uppercase">Algunas imagenes...</h2>
 				{status === STATUS.SUCCESSFUL && (
 					<div>
-						<PhotoAlbum photos={photos} layout='rows' />
-						<div className='m-0 row justify-content-center mt-2'>
-							<div className='col-auto text-center'>
+						<PhotoAlbum
+							photos={photos}
+							layout="rows"
+							onClick={(event, photo, index) => {
+								setLigthbox({ photoIndex: index, openLightbox: true })
+							}}
+						/>
+						<div className="m-0 row justify-content-center mt-2">
+							{ligthbox.openLightbox && (
+								<Lightbox
+									mainSrc={photos[ligthbox.photoIndex]?.src}
+									nextSrc={
+										photos[(ligthbox.photoIndex + 1) % photos?.length]?.src
+									}
+									prevSrc={
+										photos[
+											(ligthbox.photoIndex + photos?.length - 1) %
+												photos?.length
+										]?.src
+									}
+									onCloseRequest={() => {
+										setLigthbox({ photoIndex: 0, openLightbox: false })
+									}}
+									onMovePrevRequest={() =>
+										setLigthbox({
+											...ligthbox,
+											photoIndex:
+												(ligthbox.photoIndex + photos.length - 1) %
+												photos.length,
+										})
+									}
+									onMoveNextRequest={() =>
+										setLigthbox({
+											...ligthbox,
+											photoIndex: (ligthbox.photoIndex + 1) % photos.length,
+										})
+									}
+								/>
+							)}
+
+							<div className="col-auto text-center">
 								<Pagination
 									handlePageChange={handlePageChange}
 									total={galery.total}
@@ -66,9 +100,9 @@ const ImageEvent = () => {
 					</div>
 				)}
 				{status === STATUS.PENDING && (
-					<div className='d-flex justify-content-center '>
-						<div className='spinner-grow text-secondary ' role='status'>
-							<span className='visually-hidden'>Loading...</span>
+					<div className="d-flex justify-content-center ">
+						<div className="spinner-grow text-secondary " role="status">
+							<span className="visually-hidden">Loading...</span>
 						</div>
 					</div>
 				)}
