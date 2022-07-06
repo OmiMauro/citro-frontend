0aecaa59f0cbeeac617f974621d0564ed44a255e
Manu <ominuka.mauro@gmail.com>
2022-07-06 09:11:18 -0300

    Added form for create a new event

--- src/containers/Events/EventsFormContainer.js
+++ src/containers/Events/EventsFormContainer.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import EventForm from '../../components/Events/EventsForm'
+import {
+	selectorEvent,
+	fetchEventsById,
+	createEvent,
+	updateEvent,
+} from '../../redux/slices/event-slice'
 
 const EventsFormContainer = () => {
-	return <div>EventsFormContainer</div>
+	const { id } = useParams()
+	const dispatch = useDispatch()
+	const { event, errors, msg } = useSelector(selectorEvent)
+
+	const handleSubmit = (data) => {
+		if (id) return dispatch(updateEvent({ id, data }))
+		dispatch(createEvent(data))
+	}
+
+	useEffect(() => {
+		if (id) dispatch(fetchEventsById(id))
+	}, [dispatch])
+
+	return (
+		<>
+			<div className="col">
+				<EventForm
+					event={event}
+					errors={errors}
+					msg={msg}
+					handleSubmit={handleSubmit}
+				/>
+			</div>
+		</>
+	)
 }
 
 export default EventsFormContainer
