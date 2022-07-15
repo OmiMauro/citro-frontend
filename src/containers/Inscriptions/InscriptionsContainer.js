fcfcabc22ae6df6e8fff3386fcfc5bd59ddbabf2
Manu <ominuka.mauro@gmail.com>
2022-07-15 16:26:14 -0300

    Add pagination for view all inscriptions of a event

--- src/containers/Inscriptions/InscriptionsContainer.js
+++ src/containers/Inscriptions/InscriptionsContainer.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Inscriptions from '../../components/Inscriptions/Inscriptions'
+import Pagination from '../../components/Pagination/Pagination'
+
 import {
 	fetchInscriptionsByEvent,
 	selectorInscriptions,
@@ -10,20 +12,42 @@ const InscriptionsContainer = () => {
 	const { eventId } = useParams()
 	const { inscriptions, status, errors, msg } =
 		useSelector(selectorInscriptions)
+	const [values, setValues] = useState({
+		page: 1,
+		limit: 10,
+	})
 	const dispatch = useDispatch()
 
+	const handlePageChange = (e) => {
+		setValues({ ...values, page: e.selected + 1 })
+	}
+
 	useEffect(() => {
-		if (eventId) dispatch(fetchInscriptionsByEvent({ eventId }))
-	}, [dispatch])
+		if (eventId)
+			dispatch(
+				fetchInscriptionsByEvent({
+					eventId,
+					page: values.page,
+					limit: values.limit,
+				})
+			)
+	}, [dispatch, values])
 
 	return (
 		<div className="col">
-			<Inscriptions
-				inscriptions={inscriptions}
-				status={status}
-				errors={errors}
-				msg={msg}
-			/>
+			<div className="container">
+				<Inscriptions
+					inscriptions={inscriptions}
+					status={status}
+					errors={errors}
+					msg={msg}
+				/>
+				<Pagination
+					total={inscriptions.total}
+					pages={inscriptions.pages}
+					handlePageChange={handlePageChange}
+				/>
+			</div>
 		</div>
 	)
 }
