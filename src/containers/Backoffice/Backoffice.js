0af7f82ab9499f6292c1592d0b285fac616887c7
Manu <ominuka.mauro@gmail.com>
2022-07-15 12:20:04 -0300

    Dispatch data of user when init backoffice

--- src/containers/Backoffice/Backoffice.js
+++ src/containers/Backoffice/Backoffice.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import Tables from '../../components/Backoffice/Tables/Tables'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectorAuth } from '../../redux/slices/auth-slice'
+import { fetchUserById, selectorUsers } from '../../redux/slices/users-slice'
+
+import Home from '../../components/Backoffice/Home/Home'
 
 const Backoffice = () => {
+	const { user: userId } = useSelector(selectorAuth)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(fetchUserById(userId.userId))
+	}, [dispatch])
+
 	return (
 		<>
-			<div className='col'>
-				<Tables />
+			<div className="col">
+				<Home />
 			</div>
 		</>
 	)
