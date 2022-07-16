cc4990a8c2ffeac543e674484fdd49c697ea8100
Manu <ominuka.mauro@gmail.com>
2022-07-16 18:19:52 -0300

    Create component for that a user can register your inscription in single event

---
+++ src/components/Inscriptions/InscriptionsForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const InscriptionsForm = () => {
+	return <div>InscriptionsForm</div>
+}
+
+export default InscriptionsForm
---
+++ src/containers/Inscriptions/InscriptionsFormContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import InscriptionsForm from '../../components/Inscriptions/InscriptionsForm'
+const InscriptionsFormContainer = () => {
+	return (
+		<>
+			<div className="col">
+				<InscriptionsForm />
+			</div>
+		</>
+	)
+}
+
+export default InscriptionsFormContainer
