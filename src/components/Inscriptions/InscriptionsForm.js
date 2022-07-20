c6903457d282449e97a75d8e5cf3bd98f2430e49
Manu <ominuka.mauro@gmail.com>
2022-07-19 22:24:11 -0300

    Feat: Manage inscription of a user

--- src/components/Inscriptions/InscriptionsForm.js
+++ src/components/Inscriptions/InscriptionsForm.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react'
 
-const InscriptionsForm = ({ user, event, errors, handleSubmit, msg }) => {
+const InscriptionsForm = ({
+	user,
+	event,
+	errors,
+	handleSubmit,
+	msg,
+	provinces,
+	localities,
+	handleLocalities,
+}) => {
 	const [values, setValues] = useState({
 		_userId: user?._id ?? '',
 		_eventId: event?._id ?? '',
@@ -10,6 +19,7 @@ const InscriptionsForm = ({ user, event, errors, handleSubmit, msg }) => {
 	const handleChange = (e) => {
 		const { value, name } = e.target
 		setValues({ ...values, [name]: value })
+		if (name === 'province') handleLocalities(value)
 	}
 
 	const onSubmit = (e) => {
@@ -22,9 +32,7 @@ const InscriptionsForm = ({ user, event, errors, handleSubmit, msg }) => {
 			<div className="row justify-content-center">
 				<div className="col-md-5 border-right">
 					<div className="p-3">
-						<div className="d-flex justify-content-between align-items-center mb-3">
-							<h4 className="text-right">Registrar mi inscripción</h4>
-						</div>
+						<h4 className="text-center">Registrar mi inscripción</h4>
 						<form className="" onSubmit={onSubmit}>
 							<div className="row mt-2">
 								<div className="col-md-6">
@@ -54,7 +62,7 @@ const InscriptionsForm = ({ user, event, errors, handleSubmit, msg }) => {
 									/>
 								</div>
 							</div>
-							<div className="row mt-3">
+							<div className="row mt-1">
 								<div className="col-md-12">
 									<label htmlFor="DNI" className="form-label labels">
 										DNI
@@ -81,7 +89,75 @@ const InscriptionsForm = ({ user, event, errors, handleSubmit, msg }) => {
 										readOnly
 									/>
 								</div>
+								<div className="col-md-12">
+									<label className="form-label" htmlFor="email">
+										Email*
+									</label>
+									<input
+										type="email"
+										id="email"
+										className="form-control"
+										value={user?.email}
+										placeholder="email"
+										readOnly
+									/>
+								</div>
 							</div>
+							<div className="col-md-12">
+								<label className="form-label" htmlFor="province">
+									Provinca
+								</label>
+								<select
+									value={values.province}
+									onChange={handleChange}
+									className="form-select"
+									id="province"
+									name="province"
+									required="required"
+								>
+									<option hidden />
+									{provinces?.map((province) => (
+										<option value={province?.nombre} key={province?.id}>
+											{province?.nombre}
+										</option>
+									))}
+									<option value="Otro">No se encuentra en la lista</option>
+								</select>
+								{errors?.map(
+									(err) =>
+										err.param == 'province' && (
+											<div className="text-danger">{err.msg}</div>
+										)
+								)}
+							</div>
+							<div className="col-md-12">
+								<label className="form-label" htmlFor="province">
+									Localidad
+								</label>
+								<select
+									value={values.locality}
+									onChange={handleChange}
+									className="form-select"
+									id="locality"
+									name="locality"
+									required="required"
+								>
+									<option hidden />
+									{localities?.map((locality) => (
+										<option value={locality?.nombre} key={locality?.id}>
+											{locality?.nombre}
+										</option>
+									))}
+									<option value="Other">No se encuentra en la lista</option>
+								</select>
+								{errors?.map(
+									(err) =>
+										err.param == 'locality' && (
+											<div className="text-danger">{err.msg}</div>
+										)
+								)}
+							</div>
+
 							<div className="mt-5 text-center">
 								<button
 									type="submit"
@@ -90,7 +166,14 @@ const InscriptionsForm = ({ user, event, errors, handleSubmit, msg }) => {
 									Inscribirme
 								</button>
 							</div>
-							{msg && <p className="text-success text-center">{msg}</p>}
+							{errors?.map(
+								(err) =>
+									err.msg &&
+									!err.param && (
+										<div className="text-danger text-center">{err.msg}</div>
+									)
+							)}
+							{msg && <p className="text-center">{msg}</p>}
 						</form>
 					</div>
 				</div>
--- src/containers/Inscriptions/InscriptionsFormContainer.js
+++ src/containers/Inscriptions/InscriptionsFormContainer.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 import InscriptionsForm from '../../components/Inscriptions/InscriptionsForm'
+import {
+	selectorInscriptions,
+	createInscription,
+} from '../../redux/slices/inscriptions-slice'
+import {
+	fetchLocalities,
+	fetchProvinces,
+	selectorLocations,
+} from '../../redux/slices/location-slice'
+import { selectorUsers } from '../../redux/slices/users-slice'
+
 const InscriptionsFormContainer = () => {
+	const { eventId } = useParams()
+	const { user } = useSelector(selectorUsers)
+	const { inscriptions, status, msg, errors } =
+		useSelector(selectorInscriptions)
+	const { provinces, localities } = useSelector(selectorLocations)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(fetchProvinces({ campos: 'id,nombre' }))
+	}, [])
+
+	const handleLocalities = (province) => {
+		dispatch(fetchLocalities({ provincia: province, campos: 'id,nombre' }))
+	}
+	const handleSubmit = (data) => {
+		if (eventId) dispatch(createInscription({ _eventId: eventId, data }))
+	}
 	return (
 		<>
 			<div className="col">
-				<InscriptionsForm />
+				<InscriptionsForm
+					user={user}
+					inscriptions={inscriptions}
+					status={status}
+					msg={msg}
+					errors={errors}
+					provinces={provinces}
+					localities={localities}
+					handleLocalities={handleLocalities}
+					handleSubmit={handleSubmit}
+				/>
 			</div>
 		</>
 	)
