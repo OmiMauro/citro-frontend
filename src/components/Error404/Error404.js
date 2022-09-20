bfd15bbfcbb094250abcf6073e678f2f2cf6d26a
Manu <ominuka.mauro@gmail.com>
2022-09-20 19:40:30 -0300

    Add tag img for view image of error unauthorized

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -3,16 +3,17 @@ import Error404Image from './error-404.svg'
 import { Link } from 'react-router-dom'
 const Error404 = () => {
 	return (
-		<div className='container'>
-			<div className='row justify-content-center'>
-				<div className='col-lg-6'>
-					<div className='text-center mt-4'>
-						<img className='mb-4 img-error' src={Error404Image} />
-						<p className='lead'>
+		<div className="container">
+			<div className="row justify-content-center">
+				<div className="col-lg-6">
+					<div className="text-center mt-4">
+						<h1 className="display-1">404</h1>
+						<img className="mb-4 img-error" src={Error404Image} />
+						<p className="lead">
 							La URL ingresada no se encontró en el servidor.
 						</p>
-						<Link to='/'>
-							<i className='fas fa-arrow-left me-1'></i>
+						<Link to="/">
+							<i className="fa fa-arrow-left me-1"></i>
 							Volver al Inicio
 						</Link>
 					</div>
