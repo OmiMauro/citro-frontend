976493809e21000ad08110ae3985f265e6fdc3d0
Manu <ominuka.mauro@gmail.com>
2022-09-28 18:18:30 -0300

    Lint code of Members

--- src/components/Members/MembersList.js
+++ src/components/Members/MembersList.js
@@ -6,76 +6,86 @@ const MembersList = ({ members, handleDelete }) => {
 	}
 	return (
 		<>
-			<div className='container '>
-				<div className='row justify-content-center d-flex'>
+			<h3 className="text-center fw-bold mb-5 mx-1 mx-md-4 mt-4 text-uppercase">
+				Lista de los organizadores
+			</h3>
+			<div className="container">
+				<div className="row justify-content-center d-flex">
 					<Link
 						to={`/backoffice/members/create`}
-						className='btn btn-outline-success col-3'>
+						className="btn btn-outline-success col-3"
+					>
 						Agregar Organizador
 					</Link>
-					<Link to={`/backoffice/`} className='btn col-3 btn-outline-success '>
+					<Link to={`/backoffice/`} className="btn col-3 btn-outline-success ">
 						Volver
 					</Link>
 				</div>
-				<div className='row mt-4 justify-content-center'>
+				<div className="row mt-4 justify-content-center">
 					{members?.map((member) => {
 						return (
 							<div
-								className='card col-12 col-lg-6 m-2 align-items-center d-flex '
+								className="card col-12 col-lg-6 m-2 align-items-center d-flex "
 								key={member._id} /* style={{ width: '18rem' }} */
 							>
 								<img
 									src={member?.image_id?.url}
-									className='card-img-top m-2 w-25'
+									className="card-img-top m-2 w-25"
 									alt={member?.text}
 								/>
-								<div className='card-title'>
-									<h6 className=' mb-2 text-muted h4'>
+								<div className="card-title">
+									<h6 className=" mb-2 text-muted h4">
 										{member?.name}, {member?.lastname}
 									</h6>
 								</div>
-								<div className='card-body '>
-									<div className='d-flex justify-content-center'>
+								<div className="card-body ">
+									<div className="d-flex justify-content-center">
 										<a
-											className='card-subtitle mb-2 text-muted h-6 text-black'
+											className="card-subtitle mb-2 text-muted h-6 text-black"
 											href={member?.urlFacebook}
-											target={'_blank'}>
+											target={'_blank'}
+										>
 											{member?.urlFacebook}
 										</a>
 									</div>
-									<div className='d-flex justify-content-center'>
+									<div className="d-flex justify-content-center">
 										<a
-											className='card-subtitle mb-2 text-muted h-6 text-black'
+											className="card-subtitle mb-2 text-muted h-6 text-black"
 											href={member?.urlInstagram}
-											target={'_blank'}>
+											target={'_blank'}
+										>
 											{member?.urlInstagram}
 										</a>
 									</div>
-									<div className='d-flex justify-content-center'>
+									<div className="d-flex justify-content-center">
 										<a
-											className='card-subtitle mb-2 text-muted h-6 text-black'
+											className="card-subtitle mb-2 text-muted h-6 text-black"
 											href={member?.urlWhatsapp}
-											target={'_blank'}>
+											target={'_blank'}
+										>
 											{member?.urlWhatsapp}
 										</a>
 									</div>
-									<div className='d-flex justify-content-center'>
+									<div className="d-flex justify-content-center">
 										<a
-											className='card-subtitle mb-2 text-muted h-6 text-black'
+											className="card-subtitle mb-2 text-muted h-6 text-black"
 											href={member?.phone}
-											target={'_blank'}>
+											target={'_blank'}
+										>
 											{member?.phone}
 										</a>
 									</div>
-									<div className='card-footer text-muted justify-content-center d-flex'>
+									<div className="card-footer text-muted justify-content-center d-flex">
 										<Link
 											to={`/backoffice/members/edit/${member?._id}`}
-											className=' btn btn-outline-dark'>
+											className=" btn btn-outline-dark"
+										>
 											Editar Organizador
 										</Link>
 										<button
 											onClick={() => onDelete(member._id)}
-											className=' btn btn-outline-dark'>
+											className=" btn btn-outline-dark"
+										>
 											Eliminar Organizador
 										</button>
 									</div>
