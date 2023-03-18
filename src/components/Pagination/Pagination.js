872d36e6488d4a3df94f99f8fe1f72cd22a07b3b
Manu <ominuka.mauro@gmail.com>
2023-03-18 12:37:31 -0300

    Refactor: Pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,26 +1,32 @@
 import ReactPaginate from 'react-paginate'
 
-const Pagination = ({ handlePageChange, total, pages }) => {
+const Pagination = ({
+	handlePageChange,
+	pages = 0,
+	page = 1,
+	itemsPerPage = 20,
+}) => {
 	return (
 		<div>
 			<ReactPaginate
 				pageCount={+pages}
-				previousLabel='Anterior'
-				nextLabel='Siguiente'
-				pageClassName='page-item'
-				pageLinkClassName='page-link'
-				previousClassName='page-item'
-				previousLinkClassName='page-link'
-				nextClassName='page-item'
-				nextLinkClassName='page-link'
-				breakLabel='...'
-				breakClassName='page-item'
-				breakLinkClassName='page-link'
+				pageRangeDisplayed={itemsPerPage}
+				previousLabel="Anterior"
+				nextLabel="Siguiente"
+				pageClassName="page-item"
+				pageLinkClassName="page-link"
+				previousClassName="page-item"
+				previousLinkClassName="page-link"
+				nextClassName="page-item"
+				nextLinkClassName="page-link"
+				breakLabel="..."
+				breakClassName="page-item"
+				breakLinkClassName="page-link"
 				marginPagesDisplayed={2}
-				pageRangeDisplayed={pages}
 				onPageChange={handlePageChange}
-				containerClassName='pagination'
-				activeClassName='active'
+				containerClassName="pagination"
+				activeClassName="active"
+				forcePage={page - 1}
 			/>
 		</div>
 	)
