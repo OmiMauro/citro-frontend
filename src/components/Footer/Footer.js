b2a258d2b087dbe9fc71aed1c93e23c2676cea56
ManuOminuka <ominuka.mauro@gmail.com>
2024-01-07 18:17:49 -0300

    Feat(footer): create reusable link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,77 +1,82 @@
 import { useSelector } from 'react-redux'
 import { selectorOrganization } from '../../redux/slices/organization-slice'
 import './styles.css'
-
+import React from 'react'
+import { Row, Col, Button } from 'react-bootstrap'
+import './styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faFacebook,
+	faInstagram,
+	faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+const UrlSocial = ({ url, icon }) => {
+	if (!url) return null
+	return (
+		<Button
+			variant="dark"
+			className="btn-social mx-2"
+			href={url}
+			target="_blank"
+			rel="noreferrer"
+		>
+			<FontAwesomeIcon icon={icon} />
+		</Button>
+	)
+}
 const Footer = () => {
 	const { organization, status } = useSelector(selectorOrganization)
 	const year = new Date().getFullYear()
 	return (
-		<footer className='footer py-4'>
-			<div className='container'>
-				<div className='row align-items-center'>
-					<div className='col-lg-4 my-3 my-lg-0'>
-						<div className='mx-auto text-uppercase text-black-50'>
-							Desarrollado por &copy;Omiñuka, Mauro
-						</div>
-					</div>
-					<div className='col-lg-4  '>
-						{organization.urlFacebook && (
-							<a
-								className='btn btn-dark btn-social mx-2'
-								target='_blank'
-								rel='noreferrer'
-								href={organization.urlFacebook}>
-								<i className='fa fa-facebook' aria-hidden='true'></i>
-							</a>
-						)}
-						{organization.urlWhatsapp && (
-							<a
-								className='btn btn-dark btn-social mx-2'
-								target='_blank'
-								rel='noreferrer'
-								href={organization.urlWhatsapp}>
-								<i className='fa fa-whatsapp' aria-hidden='true'></i>
-							</a>
-						)}
-						{organization.urlInstagram && (
-							<a
-								className='btn btn-dark btn-social mx-2'
-								target='_blank'
-								rel='noreferrer'
-								href={organization.urlInstagram}>
-								<i className='fa fa-instagram'></i>
-							</a>
-						)}
-						{organization.phone && (
-							<a
-								className='btn btn-dark btn-social mx-2'
-								target='_blank'
-								rel='noreferrer'
-								href={`tel:${organization.phone}`}>
-								<i className='fa fa-phone'></i>
-							</a>
-						)}
-					</div>
-					<div className='col-lg-4 text-lg-end'>
-						<a
-							className='link-dark text-decoration-none me-3'
-							href='privacy_policy.html'
-							target='_blank'
-							rel='noreferrer'>
-							Políticas de Privacidad
-						</a>
-						<a
-							className='link-dark text-decoration-none'
-							href='terms_conditions.html'
-							target='_blank'
-							rel='noreferrer'>
-							Terminos y Condiciones
-						</a>
-						<br />
-					</div>
-				</div>
-			</div>
-		</footer>
+		<Row className="py-2 py-md-4">
+			<Col
+				xs={12}
+				lg={4}
+				className="d-flex justify-content-center align-items-start"
+			>
+				{organization.urlFacebook &&
+					UrlSocial({ url: organization.urlFacebook, icon: faFacebook })}
+				{organization.urlInstagram &&
+					UrlSocial({ url: organization.urlInstagram, icon: faInstagram })}
+				{organization.urlWhatsapp &&
+					UrlSocial({ url: organization.urlWhatsapp, icon: faWhatsapp })}
+			</Col>
+			<Col
+				xs={12}
+				lg={4}
+				className="d-flex justify-content-center align-items-center py-md-0"
+			>
+				<p className="text-uppercase text-black-50 ">
+					Desarrollado por
+					<span className="fw-bold"> &copy;Manu Omiñuka</span>
+				</p>
+			</Col>
+
+			<Col
+				xs={12}
+				lg={4}
+				className="d-flex justify-content-center align-items-start "
+			>
+				<a
+					className="link-dark text-decoration-none me-3"
+					href="privacy_policy.html"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Políticas de Privacidad
+				</a>
+				<a
+					className="link-dark text-decoration-none"
+					href="terms_conditions.html"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Terminos y Condiciones
+				</a>
+			</Col>
+		</Row>
 	)
 }
 export default Footer
