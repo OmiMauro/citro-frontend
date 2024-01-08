700a7de689275044a1f4595a83429e11e1670a7a
ManuOminuka <ominuka.mauro@gmail.com>
2024-01-08 09:09:11 -0300

    Refactor: export the setting for the editor

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,24 @@
 export const ROLES = {
 	/* SUPER_ADMIN: '1', */
 	ADMIN: 2,
-	CUSTOMER: 1
+	CUSTOMER: 1,
+}
+
+export const editorConfig = {
+	toolbar: {
+		items: [
+			'bold',
+			'italic',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'indent',
+			'outdent',
+			'|',
+			'heading',
+			'|',
+			'undo',
+			'redo',
+		],
+	},
 }
