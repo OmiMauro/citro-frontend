c8b1354c39963296c4a72690c6103edd6f6832a3
ManuOminuka <ominuka.mauro@gmail.com>
2024-01-08 09:03:07 -0300

    Feat(spinner): create a reusable component

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Spinner as BootstrapSpinner } from 'react-bootstrap'
 
 const Spinner = () => {
 	return (
-		<div className='d-flex justify-content-center my-5'>
-			<div className='spinner-grow' role='status'>
-				<span className='visually-hidden'>Loading...</span>
-			</div>
+		<div className="d-flex justify-content-center my-5">
+			<BootstrapSpinner animation="grow" role="status">
+				<span className="visually-hidden">Loading...</span>
+			</BootstrapSpinner>
 		</div>
 	)
 }
